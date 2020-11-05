4271d046639f6448d9f0237bbbbcbe1347f5dbfa
Brian Curnow <brian.curnow@gmail.com>
2020-11-05 01:31:22 +0000

    New cli bootstrap for the new cli module

---
+++ rfidsecuritysvc/bootstrap/cli.py
@@ -0,0 +1,6 @@
+import rfidsecuritysvc.cli
+from rfidsecuritysvc.util.modules import call_method_on_each
+
+def bootstrap(app):
+  """Will call the register method on each module to register their commands"""
+  call_method_on_each(rfidsecuritysvc.cli, 'register', {'app': app})
