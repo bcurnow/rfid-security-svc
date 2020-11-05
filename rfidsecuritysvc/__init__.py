3e81601e0b1f9e0565648b5df0e46ce92a26531f
Brian Curnow <brian.curnow@gmail.com>
2020-11-05 01:27:38 +0000

    Added the connexion wrapper and moved the database close teardown_appcontext hook to a bootstrapper

--- rfidsecuritysvc/__init__.py
+++ rfidsecuritysvc/__init__.py
@@ -1,9 +1,21 @@
 import os
-
+import connexion
+from connexion.resolver import RestyResolver
+from swagger_ui_bundle import swagger_ui_3_path
 from flask import Flask
 
 def create_app(test_config=None):
-  app = Flask(__name__, instance_relative_config=True)
+  # Setup Flask using the Connexion wrapper
+  options = {
+             'instance_relative_config': True,
+             'swagger_path': swagger_ui_3_path,
+            }
+  api_app = connexion.App(__name__, specification_dir='api/', options=options)
+  api_app.add_api('api.yml', strict_validation=True, validate_responses=True, resolver=RestyResolver(f'{__package__}.api'))
+
+  # Get the wrapped Flask class so we can configure Flask
+  app = api_app.app
+
   app.config.from_mapping(
     SECRET_KEY='dev',
     DATABASE=os.path.join(app.instance_path, 'rfidsecurity.sqlite'),
@@ -22,9 +34,5 @@ def create_app(test_config=None):
   # Bootstrap rfidsecuritysvc
   from rfidsecuritysvc.bootstrap import main
   main.bootstrap(app)
-
-  # Ensure that the database connection is closed when we teardown the appcontext
-  from rfidsecuritysvc.db.dbms import close_db
-  app.teardown_appcontext(close_db)
 
   return app
