a5e91620c729bf6002107802a1f6d2a44280fc4e
Brian Curnow <brian.curnow@gmail.com>
2020-11-05 22:52:09 -0500

    initial add

---
+++ rfidsecuritysvc/exception/__init__.py
@@ -0,0 +1,26 @@
+class BaseError(Exception):
+    pass
+
+class MediaNotFoundError(BaseError):
+    pass
+
+class DuplicateMediaError(BaseError):
+    pass
+
+class PermissionNotFoundError(BaseError):
+    pass
+
+class DuplicatePermissionError(BaseError):
+    pass
+
+class ConfigNotFoundError(BaseError):
+    pass
+
+class DuplicateConfigError(BaseError):
+    pass
+
+class MediaPermNotFoundError(BaseError):
+    pass
+
+class DuplicateMediaPermError(BaseError):
+    pass
