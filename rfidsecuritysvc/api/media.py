f6f34ecab9be5e0a9848b15abba6eed0aa945567
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:41:13 -0500

    Added RECORD_COUNT_HEADER support

--- rfidsecuritysvc/api/media.py
+++ rfidsecuritysvc/api/media.py
@@ -1,4 +1,5 @@
 import rfidsecuritysvc.exception as exception
+from rfidsecuritysvc.api import RECORD_COUNT_HEADER
 from rfidsecuritysvc.model import media as model
 
 def get(id):
@@ -21,14 +22,16 @@ def post(body):
         return f'Object with id "{body["id"]}" or name "{body["name"]}" already exists.', 409
 
 def delete(id):
-    model.delete(id)
-    return None, 200 
+    return None, 200, {RECORD_COUNT_HEADER: model.delete(id)}
 
 def put(id, body):
     try:
         model.update(id, body['name'], body['desc'])
         return None, 200
     except exception.MediaNotFoundError:
-        model.create(id, body['name'], body['desc'])
-        return None, 201
+        try:
+            model.create(id, body['name'], body['desc'])
+            return None, 201
+        except exception.DuplicateMediaError:
+            return f'Object with id "{id}" or name "{body["name"]}" already exists.', 409
 
