fe55c7891d85d7ab6637a566f07e564ce0f716c8
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:43:07 -0500

    Added get, delete and update by name

--- rfidsecuritysvc/db/permission.py
+++ rfidsecuritysvc/db/permission.py
@@ -5,6 +5,9 @@ import rfidsecuritysvc.exception as exception
 def get(id):
     return get_db().execute('SELECT * FROM permission WHERE id = ?', (id,)).fetchone()
 
+def get_by_name(name):
+    return get_db().execute('SELECT * FROM permission WHERE name = ?', (name,)).fetchone()
+
 def list():
     return get_db().execute('SELECT * FROM permission ORDER BY id').fetchall()
 
@@ -22,8 +25,20 @@ def delete(id):
     db.commit()
     return count
 
+def delete_by_name(name):
+    db = get_db()
+    count = db.execute('DELETE FROM permission WHERE name = ?', (name,)).rowcount
+    db.commit()
+    return count
+
 def update(id, name, desc):
     db = get_db()
     if db.execute('UPDATE permission SET name = ?, desc = ? WHERE id = ?', (name, desc, id)).rowcount == 0:
         raise exception.PermissionNotFoundError
     db.commit();
+
+def update_by_name(name, desc):
+    db = get_db()
+    if db.execute('UPDATE permission SET desc = ? WHERE NAME = ?', (desc,name)).rowcount == 0:
+        raise exception.PermissionNotFoundError
+    db.commit()
