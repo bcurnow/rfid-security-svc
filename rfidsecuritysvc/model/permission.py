d586cfabfb5d3ecd079a5b24a03fdcfde7b5ecd3
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:46:09 -0500

    Added get, delete and update by name

--- rfidsecuritysvc/model/permission.py
+++ rfidsecuritysvc/model/permission.py
@@ -2,30 +2,39 @@ from rfidsecuritysvc.db import permission as table
 from rfidsecuritysvc.model import BaseModel
 
 class Permission(BaseModel):
-  def __init__(self, id, name, desc=None):
-    self.id = id
-    self.name = name
-    self.desc = desc
+    def __init__(self, id, name, desc=None):
+        self.id = id
+        self.name = name
+        self.desc = desc
 
 def get(id):
-  row = table.get(id)
-  if not row: return
-  return __model(row)
+    return __model(table.get(id))
+
+def get_by_name(name):
+    return __model(table.get_by_name(name))
 
 def list():
-  result = []
-  for row in table.list():
-    result.append(__model(row))
-  return result  
+    result = []
+    for row in table.list():
+        result.append(__model(row))
+
+    return result  
 
 def create(name, desc):
-  return table.create(name, desc)
+    return table.create(name, desc)
 
 def delete(id):
-  return table.delete(id)
+    return table.delete(id)
+
+def delete_by_name(name):
+    return table.delete_by_name(name)
 
 def update(id, name, desc):
-  return table.update(id, name, desc)
+    return table.update(id, name, desc)
+
+def update_by_name(name, desc):
+    return table.update_by_name(name, desc)
 
 def __model(row):
-  return Permission(row['id'], row['name'], row['desc'])
+    if not row: return
+    return Permission(row['id'], row['name'], row['desc'])
