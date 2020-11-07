325ab25c64f2e51626c94e3f71403bf38db4a81f
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:44:42 -0500

    Updated formatting and moved the logic handling None into the __model method

--- rfidsecuritysvc/model/media.py
+++ rfidsecuritysvc/model/media.py
@@ -2,30 +2,30 @@ from rfidsecuritysvc.db import media as table
 from rfidsecuritysvc.model import BaseModel
 
 class Media(BaseModel):
-  def __init__(self, id, name, desc=None):
-    self.id = id
-    self.name = name
-    self.desc = desc
+    def __init__(self, id, name, desc=None):
+        self.id = id
+        self.name = name
+        self.desc = desc
 
 def get(id):
-  row = table.get(id)
-  if not row: return
-  return __model(row)
+    return __model(table.get(id))
 
 def list():
-  result = []
-  for row in table.list():
-    result.append(__model(row))
-  return result  
+    result = []
+    for row in table.list():
+        result.append(__model(row))
+
+    return result  
 
 def create(id, name, desc):
-  return table.create(id, name, desc)
+    return table.create(id, name, desc)
 
 def delete(id):
-  return table.delete(id)
+    return table.delete(id)
 
 def update(id, name, desc):
-  return table.update(id, name, desc)
+    return table.update(id, name, desc)
 
 def __model(row):
-  return Media(row['id'], row['name'], row['desc'])
+    if not row: return
+    return Media(row['id'], row['name'], row['desc'])
