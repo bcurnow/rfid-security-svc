1373a8c1cff5e1da3f447f6a88b1babda6036160
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:39:20 -0500

    Reformatted, added swagger_ui configuration

--- rfidsecuritysvc/__init__.py
+++ rfidsecuritysvc/__init__.py
@@ -7,35 +7,45 @@ from flask import Flask
 from rfidsecuritysvc.util.exception import render_unauthorized
 
 def create_app(test_config=None):
-  # Setup Flask using the Connexion wrapper
-  options = {
-             'instance_relative_config': True,
-             'swagger_path': swagger_ui_3_path,
-            }
-  api_app = connexion.App(__name__, specification_dir='api/', options=options)
-  api_app.add_api('api.yaml', strict_validation=True, validate_responses=True, resolver=RestyResolver(f'{__package__}.api'))
-  # Add a custom renderer for OAuthProblems so we set the correct headers and return meaningful messages
-  api_app.add_error_handler(OAuthProblem, render_unauthorized)
-  # Get the wrapped Flask class so we can configure Flask
-  app = api_app.app
 
-  app.config.from_mapping(
-    SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'rfidsecurity.sqlite'),
-  )
+    swagger_ui_config = {
+        'docExpansion': 'none',
+        'persistAuthorization': True,
+    }
 
-  if test_config is None:
-    app.config.from_pyfile('config.py', silent=True)
-  else:
-    app.config.from_mapping(test_config)
-  
-  try:
-    os.makedirs(app.instance_path)
-  except OSError:
-    pass
+    connexion_options = {
+        'instance_relative_config': True,
+        'swagger_path': swagger_ui_3_path,
+        'swagger_ui_config': swagger_ui_config,
+    }
 
-  # Bootstrap rfidsecuritysvc
-  from rfidsecuritysvc.bootstrap import main
-  main.bootstrap(app)
+    # Setup Flask using the Connexion wrapper
+    api_app = connexion.App(__name__, specification_dir='api/', options=connexion_options)
+    api_app.add_api('api.yaml', strict_validation=True, validate_responses=True, resolver=RestyResolver(f'{__package__}.api'))
 
-  return app
+    # Add a custom renderer for OAuthProblems so we set the correct headers and return meaningful messages
+    api_app.add_error_handler(OAuthProblem, render_unauthorized)
+
+    # Get the wrapped Flask class so we can configure Flask
+    app = api_app.app
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'rfidsecurity.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # Bootstrap rfidsecuritysvc
+    from rfidsecuritysvc.bootstrap import main
+    main.bootstrap(app)
+
+    return app
