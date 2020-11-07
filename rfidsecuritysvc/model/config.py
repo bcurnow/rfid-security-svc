a4d33d3ee0c16634447315612a5bfd3c40114f09
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:44:15 -0500

    Updated formatting and moved the logic handling None into the __model method

--- rfidsecuritysvc/model/config.py
+++ rfidsecuritysvc/model/config.py
@@ -2,29 +2,29 @@ from rfidsecuritysvc.db import config as table
 from rfidsecuritysvc.model import BaseModel
 
 class Config(BaseModel):
-  def __init__(self, key, value):
-    self.key = key
-    self.value = value
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
 
 def get(key):
-  row = table.get(key)
-  if not row: return
-  return __model(row)
+    return __model(table.get(key))
 
 def list():
-  result = []
-  for row in table.list():
-    result.append(__model(row))
-  return result  
+    result = []
+    for row in table.list():
+        result.append(__model(row))
+
+    return result  
 
 def create(key, value):
-  return table.create(key, value)
+    return table.create(key, value)
 
 def delete(key):
-  return table.delete(key)
+    return table.delete(key)
 
 def update(key, value):
-  return table.update(key, value)
+    return table.update(key, value)
 
 def __model(row):
-  return Config(row['key'], row['value'])
+    if not row: return
+    return Config(row['key'], row['value'])
