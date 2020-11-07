90d2f70df405042b2f6701e52b902999860dce85
Brian Curnow <brian.curnow@gmail.com>
2020-11-07 18:41:43 -0500

    Change password to key

--- rfidsecuritysvc/cli/auth.py
+++ rfidsecuritysvc/cli/auth.py
@@ -1,4 +1,3 @@
-
 import click
 from flask.cli import AppGroup
 import rfidsecuritysvc.exception as exception
@@ -13,6 +12,6 @@ def register(app):
 @click.pass_context
 def generate_api_key(ctx):
     """Generates a new 512 bit secure token to be used to access the API, this will replace any current key"""
-    click.confirm("Are you sure, this will invalidate the old password?", abort=True)
+    click.confirm("Are you sure, this will invalidate the old key?", abort=True)
     key = auth.generate_api_key()
     click.echo(click.style(f'Generated new key "{key}", please record this value as it is not retrievable', bg='green', fg='black'))
