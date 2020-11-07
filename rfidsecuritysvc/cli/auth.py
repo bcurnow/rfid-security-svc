48a168b1a320ebd94df9b330093d810012d39816
Brian Curnow <brian.curnow@gmail.com>
2020-11-06 20:18:27 -0500

    Added a command line to generate a new api key

---
+++ rfidsecuritysvc/cli/auth.py
@@ -0,0 +1,18 @@
+
+import click
+from flask.cli import AppGroup
+import rfidsecuritysvc.exception as exception
+from rfidsecuritysvc.api import auth
+
+group = AppGroup('auth')
+
+def register(app):
+    app.cli.add_command(group)
+
+@group.command('generate-api-key')
+@click.pass_context
+def generate_api_key(ctx):
+    """Generates a new 512 bit secure token to be used to access the API, this will replace any current key"""
+    click.confirm("Are you sure, this will invalidate the old password?", abort=True)
+    key = auth.generate_api_key()
+    click.echo(click.style(f'Generated new key "{key}", please record this value as it is not retrievable', bg='green', fg='black'))
