4977104cf3cffbc2f1350f6024385aa61f15cf80
Brian Curnow <brian.curnow@gmail.com>
2020-11-08 01:08:28 -0500

    Added authorized API and support model module

---
+++ rfidsecuritysvc/api/authorized.py
@@ -0,0 +1,10 @@
+
+from rfidsecuritysvc.model import authorized
+
+def get(media_id, perm_name):
+    if authorized.authorized(media_id, perm_name):
+        return None, 200
+
+    # Return Forbidden to indicate that they are not authorized
+    # I considered 401 but the semantics are slightly differnt and don't really match
+    return None, 403
---
+++ rfidsecuritysvc/model/authorized.py
@@ -0,0 +1,13 @@
+from rfidsecuritysvc import exception as exception
+from rfidsecuritysvc.model import association
+
+def authorized(media_id, perm_name):
+    try:
+        if association.get(media_id, perm_name):
+            return True
+        else:
+            return False
+    except exception.PermissionNotFoundError:
+        return False
+    except exception.AssociationNotFoundError:
+        return False
