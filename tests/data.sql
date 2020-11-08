16efa79d9ea2b1e4955a17a6eddbdb8c15a9afc6
Brian Curnow <brian.curnow@gmail.com>
2020-11-08 01:06:49 -0500

    Moved to the db sub-directory

--- tests/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO media (id, name, desc)
-VALUES 
-  ('test media 1', 'test media 1', 'Media for testing (1)');
-
-INSERT INTO permission (name, desc)
-VALUES
-  ('Perm 1', 'The first permission'),
-  ('Perm 2', 'The second permission');
