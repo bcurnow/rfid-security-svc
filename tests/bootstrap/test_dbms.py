6a4adba60988945f1fb72c1b621627eed05b7b7c
Brian Curnow <brian.curnow@gmail.com>
2020-11-09 13:21:04 -0500

    Added test for teardown here instead of in test_dbms.py

--- tests/bootstrap/test_dbms.py
+++ tests/bootstrap/test_dbms.py
@@ -1,3 +1,6 @@
+import pytest
+import sqlite3
+
 from flask import g
 
 from rfidsecuritysvc.db.dbms import get_db
@@ -10,3 +13,12 @@ def test_teardown_appcontext_clears_g(app):
         theglobals = g.__dict__
 
     assert 'db' not in theglobals
+
+def test_teardown_appcontext_closes_db(app):
+    with app.app_context():
+        db = get_db()
+
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        # If the connection is closed, this will generate an error
+        db.execute('SELECT 1')
+        assert 'Cannot operate on a closed database' in str(e.value)
