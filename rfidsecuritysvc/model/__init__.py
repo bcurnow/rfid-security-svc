566e4a0a581398a727dc08328edb1ea5c6e55393
Brian Curnow <brian.curnow@gmail.com>
2020-11-08 21:57:15 -0500

    Formatted and added __eq__ implementation to make testing easier

--- rfidsecuritysvc/model/__init__.py
+++ rfidsecuritysvc/model/__init__.py
@@ -1,9 +1,13 @@
 class BaseModel:
-  def to_json(self):
-    return self.__dict__
+    def to_json(self):
+        return self.__dict__
 
-  def __str__(self):
-    return str(self.__dict__)
+    def __str__(self):
+        return str(self.__dict__)
 
-  def __repr__(self):
-    return self.__str__()
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+                and self.__dict__ == other.__dict__)
