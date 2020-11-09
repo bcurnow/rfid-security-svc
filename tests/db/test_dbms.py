a9b23fd195df393250fcd92bce0d30679b4efdc2
Brian Curnow <brian.curnow@gmail.com>
2020-11-09 13:21:25 -0500

    Switched to actual unit test

--- tests/db/test_dbms.py
+++ tests/db/test_dbms.py
@@ -1,31 +1,40 @@
 import sqlite3
 import pytest
 
-from flask import g, appcontext_popped, appcontext_pushed
+from unittest.mock import Mock, patch
+
+from flask import g
 
 from rfidsecuritysvc.db.dbms import get_db
 
-def test_get_db_creates_single_connection(app):
+@patch('rfidsecuritysvc.db.dbms.sqlite3')
+def test_get_db(sqlite3, app):
     with app.app_context():
-        db = get_db()
-        assert db is get_db()
+        connection = Mock()
+        sqlite3.connect.return_value = connection
+        connection.execute.return_value = None
+        get_db()
+        sqlite3.connect.assert_called_once_with(app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
+        assert connection.row_factory == sqlite3.Row
+        connection.execute.assert_called_once_with('PRAGMA foreign_keys = ON')
 
-def test_get_db_enables_foreign_keys(app):
+@patch('rfidsecuritysvc.db.dbms.sqlite3')
+def test_get_db_creates_single_connection(sqlite3, app):
     with app.app_context():
-        db = get_db()
-        assert db.execute('PRAGMA foreign_keys').fetchone()['foreign_keys'] == 1
+        assert get_db() == get_db()
 
-def test_get_db_configures_sqlite3_row(app):
-    with app.app_context():
-        db = get_db()
-        assert db.row_factory == sqlite3.Row
-
-def test_autoclose_closes_connection(app):
+def test_close_db(app):
     with app.app_context():
         db = get_db()
 
     with pytest.raises(sqlite3.ProgrammingError) as e:
+        # If the connection is closed, this will generate an error
         db.execute('SELECT 1')
+        assert 'Cannot operate on a closed database' in str(e.value)
 
-    assert 'Cannot operate on a closed database' in str(e.value)
+def test_close_db_clears_g(app):
+    with app.app_context():
+        get_db()
+        theg = g.__dict__
 
+    assert 'db' not in theg
