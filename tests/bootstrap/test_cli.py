f1792bf0b84a14a0f780862779a6f37ed1e4182e
Brian Curnow <brian.curnow@gmail.com>
2020-11-09 13:21:57 -0500

    Added test to ensure the cli bootstrapper added all the top-level commands

---
+++ tests/bootstrap/test_cli.py
@@ -0,0 +1,7 @@
+
+def test_bootstrap_registers_cli_group(app):
+    """This test will simply ensure that all the expected top-level groups are available, test code for each cli module will do the rest"""
+    with app.app_context():
+        expected = ['auth', 'config', 'db', 'media', 'media-perm', 'permission', 'test']
+        for cmd in app.cli.list_commands(app.app_context()):
+            assert cmd in expected
