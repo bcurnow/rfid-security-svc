56d9c4026b72507bfe1e9689a62c397dd549a320
Brian Curnow <brian.curnow@gmail.com>
2020-11-10 13:08:46 -0500

    Added missing error handling for DuplicatePermissionError on a update

--- rfidsecuritysvc/api/permission.py
+++ rfidsecuritysvc/api/permission.py
@@ -28,5 +28,8 @@ def put(name, body):
     try:
         return None, 200, {RECORD_COUNT_HEADER: model.update_by_name(name, body['desc'])}
     except exception.PermissionNotFoundError:
-        model.create(name, body['desc'])
-        return None, 201, {RECORD_COUNT_HEADER: 1}
+        try:
+            model.create(name, body['desc'])
+            return None, 201, {RECORD_COUNT_HEADER: 1}
+        except exception.DuplicatePermissionError:
+            return f'Object with name "{name}" already exists.', 409
