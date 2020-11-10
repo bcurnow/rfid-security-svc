95371902062e3d76cca2d918b832e6de291f6d61
Brian Curnow <brian.curnow@gmail.com>
2020-11-10 18:27:05 -0500

    Had the tests mis-named

--- tests/api/test_config.py
+++ tests/api/test_config.py
@@ -1,16 +1,71 @@
 import pytest
 from unittest.mock import patch
 
-from rfidsecuritysvc.api import authorized as api
+from rfidsecuritysvc.api import RECORD_COUNT_HEADER
+from rfidsecuritysvc.api import config as api
+from rfidsecuritysvc.exception import DuplicateConfigError as DuplicateError
+from rfidsecuritysvc.exception import ConfigNotFoundError as NotFoundError
+from rfidsecuritysvc.model.config import Config as Model
 
-@patch('rfidsecuritysvc.api.authorized.model')
-def test_get_authorized(model):
-    model.authorized.return_value = True
-    assert api.get('test', 1) == (None, 200)
-    model.authorized.assert_called_once_with('test', 1)
+m = Model('key', 'value')
 
-@patch('rfidsecuritysvc.api.authorized.model')
-def test_get_not_authorized(model):
-    model.authorized.return_value = False
-    assert api.get('test', 1) == (None, 403)
-    model.authorized.assert_called_once_with('test', 1)
+@patch('rfidsecuritysvc.api.config.model')
+def test_get(model):
+    model.get.return_value = m
+    assert api.get(m.key)  == m.to_json()
+    model.get.assert_called_once_with(m.key)
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_get_notfound(model):
+    model.get.return_value = None
+    assert api.get(m.key) == (f'Object with key "{m.key}" does not exist.', 404)
+    model.get.assert_called_once_with(m.key)
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_search(model):
+    m2 = Model('key2', 'value2')
+    model.list.return_value = [m, m2]
+    assert api.search() == [m.to_json(), m2.to_json()]
+    model.list.assert_called_once()
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_search_noresults(model):
+    model.list.return_value = []
+    assert api.search() == []
+    model.list.assert_called_once()
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_post(model):
+    model.create.return_value = None
+    assert api.post(m.to_json()) == (None, 201)
+    model.create.assert_called_once_with(**m.to_json())
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_post_Duplicate(model):
+    model.create.side_effect = DuplicateError
+    assert api.post(m.to_json()) == (f'Object with key "{m.key}" already exists.', 409)
+    model.create.assert_called_once_with(**m.to_json())
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_delete(model):
+    model.delete.return_value = 1
+    assert api.delete(m.key) == (None, 200, {RECORD_COUNT_HEADER: 1})
+    model.delete.assert_called_once_with(m.key)
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_put(model):
+    model.update.return_value = 1
+    assert api.put(m.key, _update(m)) == (None, 200, {RECORD_COUNT_HEADER: 1})
+    model.update.assert_called_once_with(m.key, m.value)
+
+@patch('rfidsecuritysvc.api.config.model')
+def test_put_already_exists(model):
+    model.update.side_effect = NotFoundError
+    assert api.put(m.key, _update(m)) == (None, 201, {RECORD_COUNT_HEADER: 1})
+    model.update.assert_called_once_with(m.key, m.value)
+    model.create.assert_called_once_with(m.key, m.value)
+
+def _update(m):
+    d = m.to_json().copy()
+    d.pop('key')
+    return d
