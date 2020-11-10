b029ceddd14436c597438556ea08d775f5d567ea
Brian Curnow <brian.curnow@gmail.com>
2020-11-10 13:09:54 -0500

    Added second field to ensure that multiple fields rendered correctly, added test to ensure that to_json returns a copy of the __dict__

--- tests/model/test_init.py
+++ tests/model/test_init.py
@@ -1,17 +1,25 @@
 from rfidsecuritysvc.model import BaseModel
 
 class ExampleModel(BaseModel):
-    def __init__(self, test):
-        self.test = test
+    def __init__(self, field1, field2):
+        self.field1 = field1
+        self.field2 = field2
 
 def test_BaseModel_to_json():
-    em = ExampleModel('tojson')
-    assert em.to_json() == {'test': 'tojson'}
+    em = ExampleModel('tojson', 'tojson2')
+    assert em.to_json() == {'field1': 'tojson', 'field2': 'tojson2'}
+
+def test_BaseModel_to_json_returns_copy_of_dict():
+    em = ExampleModel('tojson', 'tojson2')
+    assert em.field1 is not None
+    d = em.to_json()
+    d.pop('field1')
+    assert em.field1 is not None
 
 def test_BaseModel_str():
-    em = ExampleModel('__str__')
-    assert em.__str__() == str({'test': '__str__'})
+    em = ExampleModel('__str__', 'field2')
+    assert em.__str__() == str({'field1': '__str__', 'field2': 'field2'})
 
 def test_BaseModel_repr():
-    em = ExampleModel('__repr__')
-    assert em.__repr__() == str({'test': '__repr__'})
+    em = ExampleModel('__repr__', 'field2')
+    assert em.__repr__() == str({'field1': '__repr__', 'field2': 'field2'})
