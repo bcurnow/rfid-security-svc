4baf046623a2282544a6567ce8541e4a4ee48650
Brian Curnow <brian.curnow@gmail.com>
2020-11-10 21:31:05 -0500

    Replaced most of the tests (which were already duplicated in db/test_dbms.py) with code that acutally tests if the bootstrapper did what it was supposed to

--- tests/bootstrap/test_dbms.py
+++ tests/bootstrap/test_dbms.py
@@ -1,24 +1,8 @@
-import pytest
-import sqlite3
+from rfidsecuritysvc.db.dbms import close_db
 
-from flask import g
+def test_teardown_appcontext(app):
+    # Make sure the close_db function is registered
+    assert close_db in app.teardown_appcontext_funcs
 
-from rfidsecuritysvc.db.dbms import get_db
 
-def test_teardown_appcontext_clears_g(app):
-    with app.app_context():
-        get_db()
-        assert 'db' in g
-        # Get a copy of the internal dict for g since we won't be able to use the 'g' object after the context closes
-        theglobals = g.__dict__
 
-    assert 'db' not in theglobals
-
-def test_teardown_appcontext_closes_db(app):
-    with app.app_context():
-        db = get_db()
-
-    with pytest.raises(sqlite3.ProgrammingError) as e:
-        # If the connection is closed, this will generate an error
-        db.execute('SELECT 1')
-        assert 'Cannot operate on a closed database' in str(e.value)
