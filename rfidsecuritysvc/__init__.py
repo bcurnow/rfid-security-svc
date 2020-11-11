35916e26a972cd509df632230c9190a2ecce2c0b
Brian Curnow <brian.curnow@gmail.com>
2020-11-10 21:30:10 -0500

    Added some code for testing, turned off sorting of keys when returning JSON

--- rfidsecuritysvc/__init__.py
+++ rfidsecuritysvc/__init__.py
@@ -21,18 +21,19 @@ def create_app(test_config=None):
 
     # Setup Flask using the Connexion wrapper
     api_app = connexion.App(__name__, specification_dir='api/', options=connexion_options)
-    api_app.add_api('api.yaml', strict_validation=True, validate_responses=True, resolver=RestyResolver(f'{__package__}.api'))
+    api = api_app.add_api('api.yaml', strict_validation=True, validate_responses=True, resolver=RestyResolver(f'{__package__}.api'))
 
     # Add a custom renderer for OAuthProblems so we set the correct headers and return meaningful messages
     api_app.add_error_handler(OAuthProblem, render_unauthorized)
 
     # Get the wrapped Flask class so we can configure Flask
     app = api_app.app
-
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'rfidsecurity.sqlite'),
-    )
+    
+    app.config.from_mapping({
+        'SECRET_KEY': 'dev',
+        'DATABASE': os.path.join(app.instance_path, 'rfidsecurity.sqlite'),
+        'JSON_SORT_KEYS': False,
+    })
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
@@ -43,6 +44,12 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
+
+    if app.testing:
+        # Store the connexion wrapper for tests to use
+        app.connexion = api_app
+        # Story the API for tests to use
+        app.api_yaml = api
 
     # Bootstrap rfidsecuritysvc
     from rfidsecuritysvc.bootstrap import bootstrap
