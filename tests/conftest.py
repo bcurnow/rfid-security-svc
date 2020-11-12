1edb9511b8d59e2f7cd0dd9cc4bde768d7eabdae
Brian Curnow <brian.curnow@gmail.com>
2020-11-11 22:33:24 -0500

    Added exit_code checking

--- tests/conftest.py
+++ tests/conftest.py
@@ -159,7 +159,8 @@ def mockdb(request, monkeypatch):
     
 @pytest.fixture
 def assert_output():
-    def assert_output(result, msg, **style):
+    def assert_output(result, msg, exit_code=0, **style):
+        assert result.exit_code == exit_code
         if style:
             assert bytes(click.style(str(msg), **style), 'UTF-8') in result.stdout_bytes
         else:
