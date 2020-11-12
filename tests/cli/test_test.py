d87ac12b770777352241ad39cf0e7fbe029f4d90
Brian Curnow <brian.curnow@gmail.com>
2020-11-11 22:33:13 -0500

    Added use of assert_output fixture, added missing parameter value check

--- tests/cli/test_test.py
+++ tests/cli/test_test.py
@@ -6,13 +6,13 @@ from click.exceptions import MissingParameter
 from rfidsecuritysvc.cli.test import generate_api_key
 
 @patch('rfidsecuritysvc.cli.test.generate_password_hash')
-def test_generate_test_apikey_value(generate_password_hash, runner):
+def test_generate_test_apikey_value(generate_password_hash, runner, assert_output):
     generate_password_hash.return_value = 'test'
     result = runner.invoke(args=['test', 'generate-test-apikey', 'test'])
-    assert result.output == 'test\n'
+    assert_output(result, 'test')
     generate_password_hash.assert_called_once_with('test')
 
 def test_generate_test_apikey_value_required():
-    with pytest.raises(MissingParameter):
+    with pytest.raises(MissingParameter, match='missing parameter: value'):
         generate_api_key.make_context('generate-api-key',[])
 
