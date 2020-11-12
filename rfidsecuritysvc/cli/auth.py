fb6194aeba147696228a9b4cd30c4d816bbe1736
Brian Curnow <brian.curnow@gmail.com>
2020-11-11 22:27:36 -0500

    Added missing period

--- rfidsecuritysvc/cli/auth.py
+++ rfidsecuritysvc/cli/auth.py
@@ -14,4 +14,4 @@ def generate_api_key(ctx):
     """Generates a new 512 bit secure token to be used to access the API, this will replace any current key"""
     click.confirm("Are you sure, this will invalidate the old key?", abort=True)
     key = auth.generate_api_key()
-    click.echo(click.style(f'Generated new key "{key}", please record this value as it is not retrievable', bg='green', fg='black'))
+    click.echo(click.style(f'Generated new key "{key}", please record this value as it is not retrievable.', bg='green', fg='black'))
