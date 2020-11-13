478896dc572dd39087a09b6e7a0dcd786d486d61
Brian Curnow <brian.curnow@gmail.com>
2020-11-13 00:57:29 -0500

    Reformatted the SQL an unindented since we use this as a key in testing

--- rfidsecuritysvc/db/association.py
+++ rfidsecuritysvc/db/association.py
@@ -1,13 +1,15 @@
+import textwrap
+
 from rfidsecuritysvc.db.dbms import get_db
 
 
 def list():
-    return get_db().execute('''
-                            SELECT
-                            media_id,
-                            permission.name as perm_name
-                            FROM
-                            media_perm
-                            INNER JOIN permission ON permission.id = media_perm.perm_id
-                            ORDER BY media_perm.id
-                            ''').fetchall()
+    return get_db().execute(textwrap.dedent('''
+                                            SELECT
+                                            media_id,
+                                            permission.name as perm_name
+                                            FROM
+                                            media_perm
+                                            INNER JOIN permission ON permission.id = media_perm.perm_id
+                                            ORDER BY media_perm.id
+                                            ''').replace('\n', ' ')).fetchall()
