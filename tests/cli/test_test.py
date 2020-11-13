e8fa6294e3f97d8fe8d7718c7e038d1fddbfc43f
Brian Curnow <brian.curnow@gmail.com>
2020-11-12 20:40:56 -0500

    Added dash (-) in between api and key to make it consistent with the auth subcommand

--- tests/cli/test_test.py
+++ tests/cli/test_test.py
@@ -7,13 +7,13 @@ from rfidsecuritysvc.cli.test import generate_api_key
 
 
 @patch('rfidsecuritysvc.cli.test.generate_password_hash')
-def test_generate_test_apikey_value(generate_password_hash, runner, assert_output):
+def test_generate_test_api_key_value(generate_password_hash, runner, assert_output):
     generate_password_hash.return_value = 'test'
-    result = runner.invoke(args=['test', 'generate-test-apikey', 'test'])
+    result = runner.invoke(args=['test', 'generate-test-api-key', 'test'])
     assert_output(result, 'test')
     generate_password_hash.assert_called_once_with('test')
 
 
-def test_generate_test_apikey_value_required():
+def test_generate_test_api_key_value_required():
     with pytest.raises(MissingParameter, match='missing parameter: value'):
         generate_api_key.make_context('generate-api-key', [])
