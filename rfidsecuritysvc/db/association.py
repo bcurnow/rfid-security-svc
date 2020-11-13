d17fab7b4660fd6d9605b228b2625fc08e1a616f
Brian Curnow <brian.curnow@gmail.com>
2020-11-13 00:31:37 -0500

    Reformatted a long SQL statement

--- rfidsecuritysvc/db/association.py
+++ rfidsecuritysvc/db/association.py
@@ -2,6 +2,12 @@ from rfidsecuritysvc.db.dbms import get_db
 
 
 def list():
-    return get_db().execute(
-        'SELECT media_id, permission.name as perm_name from media_perm INNER JOIN permission ON permission.id = media_perm.perm_id'
-    ).fetchall()
+    return get_db().execute('''
+                            SELECT
+                            media_id,
+                            permission.name as perm_name
+                            FROM
+                            media_perm
+                            INNER JOIN permission ON permission.id = media_perm.perm_id
+                            ORDER BY media_perm.id
+                            ''').fetchall()
