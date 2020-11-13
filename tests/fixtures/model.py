9f477a8392ad1f05728d2b61e9e92643843bb947
Brian Curnow <brian.curnow@gmail.com>
2020-11-13 01:06:00 -0500

    Moved to session scope, re-ordered some the records to ensure they match the SQL used for listing, added monkeypatching of to_json_rw method into Permission and MediaPerm

--- tests/fixtures/model.py
+++ tests/fixtures/model.py
@@ -1,32 +1,43 @@
 import pytest
 
 
+import rfidsecuritysvc
+from rfidsecuritysvc.model.association import Association
 from rfidsecuritysvc.model.config import Config
 from rfidsecuritysvc.model.media import Media
 from rfidsecuritysvc.model.media_perm import MediaPerm
 from rfidsecuritysvc.model.permission import Permission
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def configs(test_api_key):
     return [Config('ADMIN_API_KEY', test_api_key)]
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
+def creatable_config():
+    return Config('creatable key', 'creatable value')
+
+
+@pytest.fixture(scope='session')
 def medias():
     return [
-        Media('test open door', 'test open door', 'This media will be assigned the permission Open Door'),
         Media('test media 1', 'test media 1', 'Media for testing (1)'),
         Media('test media 2', 'test media 2', 'Media for testing (2)'),
         Media('test media 3', 'test media 3', 'Media for testing (3)'),
         Media('test media 4', 'test media 4', 'Media for testing (4)'),
         Media('test media 5', 'test media 5', 'Media for testing (5)'),
+        Media('test open door', 'test open door', 'This media will be assigned the permission Open Door'),
     ]
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
+def creatable_media():
+    return Media('creatable id', 'creatable name', 'creatable desc')
+
+
+@pytest.fixture(scope='session')
 def permissions():
-    """Returns a list of permission objects as defined in data.sql"""
     return [
         Permission(1, 'Open Door', 'Opens the door'),
         Permission(2, 'Perm 1', 'Permission 1'),
@@ -37,31 +48,56 @@ def permissions():
     ]
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
+def creatable_permission():
+    return Permission(7, 'creatable name', 'creatable desc')
+
+
+@pytest.fixture(scope='session')
 def media_perms(medias, permissions):
-    """Returns a list of media_perm objects as defined in data.sql minus the open door records"""
     return [
-        MediaPerm(0, medias[0].id, permissions[0].id),
-        MediaPerm(1, 'test media 1', 2),
-        MediaPerm(2, 'test media 2', 3),
-        MediaPerm(3, 'test media 3', 4),
-        MediaPerm(4, 'test media 4', 5),
-        MediaPerm(5, 'test media 5', 6),
+        MediaPerm(1, medias[5].id, permissions[0].id),
+        MediaPerm(2, 'test media 1', 2),
+        MediaPerm(3, 'test media 2', 3),
+        MediaPerm(4, 'test media 3', 4),
+        MediaPerm(5, 'test media 4', 5),
+        MediaPerm(6, 'test media 5', 6),
     ]
 
-@pytest.fixture
+
+@pytest.fixture(scope='session')
+def creatable_media_perm():
+    return MediaPerm(7, 'test media 1', 3)
+
+
+@pytest.fixture(scope='session')
+def associations(medias, permissions):
+    return [
+        Association(medias[5].id, permissions[0].name),
+        Association(medias[0].id, permissions[1].name),
+        Association(medias[1].id, permissions[2].name),
+        Association(medias[2].id, permissions[3].name),
+        Association(medias[3].id, permissions[4].name),
+        Association(medias[4].id, permissions[5].name),
+
+    ]
+
+
+@pytest.fixture(scope='session')
+def creatable_association():
+    return Association('test media 1', 'Perm 4')
+
+
+@pytest.fixture(scope='session')
 def open_door(medias, permissions, media_perms):
     """
     This fixture returns a tuple containing the Media, Permission and MediaPerm objects corresponding to the open door test
-    data in data.sql
+    data
     """
-    media = Media('test open door', 'test open door', 'This media will be assigned the permission Open Door')
-    permission = Permission('Open Door', 'Opens the door')
-    media_perm = MediaPerm('test open door', 1)
-    return (medias[0], permissions[0], media_perms[0])
+    return (medias[5], permissions[0], media_perms[0])
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def assert_model():
     def go(expected, actual):
         _assert_keys(expected, actual)
@@ -71,6 +107,40 @@ def assert_model():
 
 
 def _assert_keys(one, two):
-    for k,v in one.items():
+    for k, v in one.items():
         assert k in two
         assert two[k] == v
+
+
+@pytest.fixture(autouse=True)
+def add_to_json_rw(monkeypatch):
+    """
+    Patches the rfidsecuritysvc.model.BaseModel class to add a to_json_rw method that returns
+    only the keys that are not marked readonly in the API.
+    Adds implementation to Permission and MediaPerm
+    """
+
+    def _read_only_keys(self):
+        """
+        Identifies any keys on this object that are defined read only at the API tier.
+        Subclasses should override to specify their keys.
+        """
+        return []
+
+    def to_json_rw(self):
+        """ Returns a JSON compatible value stripped of keys which are defined read only at the API."""
+        copy = self.__dict__.copy()
+        for key in self._read_only_keys():
+            del copy[key]
+
+        return copy
+
+    monkeypatch.setattr(rfidsecuritysvc.model.BaseModel, '_read_only_keys', _read_only_keys, raising=False)
+    monkeypatch.setattr(rfidsecuritysvc.model.BaseModel, 'to_json_rw', to_json_rw, raising=False)
+
+    # Create an override method for Permission and MediaPerm
+    def remove_id(self):
+        return ['id']
+
+    monkeypatch.setattr(rfidsecuritysvc.model.permission.Permission, '_read_only_keys', remove_id, raising=False)
+    monkeypatch.setattr(rfidsecuritysvc.model.media_perm.MediaPerm, '_read_only_keys', remove_id, raising=False)
