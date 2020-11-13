e14328bcea4fb3fb13f59b984d4b3a8ce7cdcbc7
Brian Curnow <brian.curnow@gmail.com>
2020-11-13 01:06:22 -0500

    Removed the to_json_rw testing as this is no longer part of the BaseModel

--- tests/model/test_init.py
+++ tests/model/test_init.py
@@ -7,10 +7,6 @@ class ExampleModel(BaseModel):
         self.field2 = field2
 
 
-    def _read_only_keys(self):
-        return ['field2']
-
-
 def test_BaseModel_to_json():
     em = ExampleModel('tojson', 'tojson2')
     assert em.to_json() == {'field1': 'tojson', 'field2': 'tojson2'}
@@ -20,19 +16,6 @@ def test_BaseModel_to_json_returns_copy_of_dict():
     em = ExampleModel('tojson', 'tojson2')
     assert em.field1 is not None
     d = em.to_json()
-    d.pop('field1')
-    assert em.field1 is not None
-
-
-def test_BaseModel_to_json_rw():
-    em = ExampleModel('field1', 'field2')
-    assert em.to_json_rw() == {'field1': 'field1'}
-
-
-def test_BaseModel_to_json_rw_returns_copy_of_dict():
-    em = ExampleModel('tojson', 'tojson2')
-    assert em.field1 is not None
-    d = em.to_json_rw()
     d.pop('field1')
     assert em.field1 is not None
 
