5bca91ef467163179c41f01eb52954d3bd0caa00
Brian Curnow <bcurnow@users.noreply.github.com>
2020-11-15 02:43:50 -0500

    Updated to use get_connection, added testing for init_db

--- tests/db/test_dbms.py
+++ tests/db/test_dbms.py
@@ -5,30 +5,32 @@ from unittest.mock import Mock, patch
 
 from flask import g
 
-from rfidsecuritysvc.db.dbms import get_db
+from rfidsecuritysvc.db.dbms import get_connection, close_db, init_db
 
 
 @patch('rfidsecuritysvc.db.dbms.sqlite3')
-def test_get_db(sqlite3, app):
+def test_get_connection(sqlite3, app):
     with app.app_context():
         connection = Mock()
         sqlite3.connect.return_value = connection
         connection.execute.return_value = None
-        get_db()
+        get_connection()
         sqlite3.connect.assert_called_once_with(app.config['DATABASE'], detect_types=sqlite3.PARSE_DECLTYPES)
         assert connection.row_factory == sqlite3.Row
         connection.execute.assert_called_once_with('PRAGMA foreign_keys = ON')
 
 
 @patch('rfidsecuritysvc.db.dbms.sqlite3')
-def test_get_db_creates_single_connection(sqlite3, app):
+def test_connection_creates_single_connection(sqlite3, app):
     with app.app_context():
-        assert get_db() == get_db()
+        assert get_connection() == get_connection()
 
 
 def test_close_db(app):
     with app.app_context():
-        db = get_db()
+        db = get_connection()
+        db.execute('SELECT 1')
+        close_db()
 
     with pytest.raises(sqlite3.ProgrammingError) as e:
         # If the connection is closed, this will generate an error
@@ -38,7 +40,38 @@ def test_close_db(app):
 
 def test_close_db_clears_g(app):
     with app.app_context():
-        get_db()
+        get_connection()
+        assert 'db' in g
         theg = g.__dict__
 
     assert 'db' not in theg
+
+
+def test_init_db(app):
+    with app.app_context():
+        table_names = ['config', 'media', 'media_perm', 'permission']
+        connection = get_connection()
+
+        # Start by making sure all the tables exist
+        results = connection.execute('SELECT name FROM sqlite_master WHERE type = ? AND name NOT LIKE ?', ('table', 'sqlite_%')).fetchall()
+        assert len(results) == len(table_names)
+        for row in results:
+            assert row['name'] in table_names
+
+        # Now turn off foreign keys and drop the tables
+        connection.execute('PRAGMA foreign_keys = OFF')
+        for table in table_names:
+            connection.execute(f'DROP TABLE {table}')
+
+        # Verify the tables are gone
+        results = connection.execute('SELECT name FROM sqlite_master WHERE type = ? AND name NOT LIKE ?', ('table', 'sqlite_%')).fetchall()
+        assert len(results) == 0
+
+        # Re-init the database
+        init_db()
+
+        # Validate that the tables have been re-created
+        results = connection.execute('SELECT name FROM sqlite_master WHERE type = ? AND name NOT LIKE ?', ('table', 'sqlite_%')).fetchall()
+        assert len(results) == len(table_names)
+        for row in results:
+            assert row['name'] in table_names
