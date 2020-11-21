1c8e5c252caa5f25c18961d6cb900fb98033446b
Brian Curnow <bcurnow@users.noreply.github.com>
2020-11-21 15:08:14 -0500

    Added /dev/input/event0 pass through

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,4 +4,4 @@ rootDir=$(dirname $0)
 rootDir=$(cd ${rootDir}/.. && pwd)
 targetDir=$(cd ${rootDir} && basename $(pwd))
 
-docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind ${targetDir}:latest /bin/bash
+docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind --device=/dev/input/event0 ${targetDir}:latest /bin/bash
