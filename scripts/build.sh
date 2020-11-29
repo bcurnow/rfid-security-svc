28678e049b6c8256cef8b0d1d8866c541ec08f56
Brian Curnow <bcurnow@users.noreply.github.com>
2020-11-29 08:47:10 +0000

    Added input group

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,5 +7,6 @@ imageName=$(cd ${rootDir} && basename $(pwd))
 docker image build \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg GROUP_ID=$(id -g ${USER}) \
+  --build-arg INPUT_GROUP_ID=$(getent group input | cut -d: -f3) \
   -t ${imageName}:latest  \
   ${rootDir}
