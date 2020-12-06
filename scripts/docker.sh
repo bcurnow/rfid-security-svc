a1c5a1e4fd0ac6d56bb5d2530bf77d202a1d101a
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-06 14:54:42 +0000

    Added guard to ensure that we don't try to load in the device when it's not plugged in.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,4 +4,10 @@ rootDir=$(dirname $0)
 rootDir=$(cd ${rootDir}/.. && pwd)
 targetDir=$(cd ${rootDir} && basename $(pwd))
 
-docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind --device=/dev/input/rfid:/dev/input/rfid:r ${targetDir}:latest /bin/bash
+if [[ -e /dev/input/rfid && -c /dev/input/rfid ]]
+then
+	docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind --device=/dev/input/rfid:/dev/input/rfid:r ${targetDir}:latest /bin/bash
+else
+	echo "Did not find /dev/input/rfid, skipping --device"
+	docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind ${targetDir}:latest /bin/bash
+fi
