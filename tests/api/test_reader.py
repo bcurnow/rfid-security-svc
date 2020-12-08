8b8558c2d5fe7acfef094f3927e77cc05df652f2
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-08 20:10:00 +0000

    Updated test to use all uppercase

--- tests/api/test_reader.py
+++ tests/api/test_reader.py
@@ -5,7 +5,7 @@ from rfidsecuritysvc.api import reader as api
 
 @patch('rfidsecuritysvc.api.reader.reader')
 def test_search(reader):
-    reader.read.return_value = '80558aba5c3504'
+    reader.read.return_value = '80558ABA5C3504'
     assert api.search() == reader.read.return_value
     reader.read.assert_called_once_with(10)
 
@@ -19,6 +19,6 @@ def test_search_noresult(reader):
 
 @patch('rfidsecuritysvc.api.reader.reader')
 def test_search_timeout(reader):
-    reader.read.return_value = '80558aba5c3504'
+    reader.read.return_value = '80558ABA5C3504'
     assert api.search(25) == reader.read.return_value
     reader.read.assert_called_once_with(25)
