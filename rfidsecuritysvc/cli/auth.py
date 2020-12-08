2326e0811d054d77e1d74b8c1a74ec7c033f035e
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-08 21:26:05 +0000

    Switched to confirmation option

--- rfidsecuritysvc/cli/auth.py
+++ rfidsecuritysvc/cli/auth.py
@@ -10,9 +10,9 @@ def register(app):
 
 
 @group.command('generate-api-key')
+@click.confirmation_option(prompt='Are you sure, this will invalidate the old key?')
 @click.pass_context
 def generate_api_key(ctx):
     """Generates a new 512 bit secure token to be used to access the API, this will replace any current key"""
-    click.confirm("Are you sure, this will invalidate the old key?", abort=True)
     key = auth.generate_api_key()
     click.echo(click.style(f'Generated new key "{key}", please record this value as it is not retrievable.', bg='green', fg='black'))
--- tests/cli/test_auth.py
+++ tests/cli/test_auth.py
@@ -11,6 +11,15 @@ def test_generate_api_key(auth, runner, assert_output):
 
 
 @patch('rfidsecuritysvc.cli.auth.auth')
+def test_generate_api_key_with_confirm_option(auth, runner, assert_output):
+    auth.generate_api_key.return_value = 'test'
+    result = runner.invoke(args=['auth', 'generate-api-key', '--yes'], color=True)
+    assert 'Are you sure, this will invalidate the old key?' not in result.output
+    assert_output(result, 'Generated new key "test", please record this value as it is not retrievable.', bg='green', fg='black')
+    auth.generate_api_key.assert_called_once()
+
+
+@patch('rfidsecuritysvc.cli.auth.auth')
 def test_generate_api_key_no(auth, runner, assert_output):
     result = runner.invoke(args=['auth', 'generate-api-key'], input='n', color=True)
     assert_output(result, 'Are you sure, this will invalidate the old key?', 1)
