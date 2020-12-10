e55361166331b294c637ad482fc40e50dd3b8fed
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-10 23:07:58 +0000

    Removed add_to_json_rw as it's primarily for api testing.

--- tests/fixtures/model.py
+++ tests/fixtures/model.py
@@ -30,37 +30,3 @@ def _assert_keys(one, two):
     for k, v in one.items():
         assert k in two
         assert two[k] == v
-
-
-@pytest.fixture(autouse=True)
-def add_to_json_rw(monkeypatch):
-    """
-    Patches the rfidsecuritysvc.model.BaseModel class to add a to_json_rw method that returns
-    only the keys that are not marked readonly in the API.
-    Adds implementation to Permission and MediaPerm
-    """
-
-    def _read_only_keys(self):
-        """
-        Identifies any keys on this object that are defined read only at the API tier.
-        Subclasses should override to specify their keys.
-        """
-        return []
-
-    def to_json_rw(self):
-        """ Returns a JSON compatible value stripped of keys which are defined read only at the API."""
-        copy = self.__dict__.copy()
-        for key in self._read_only_keys():
-            del copy[key]
-
-        return copy
-
-    models_to_patch = {
-        rfidsecuritysvc.model.permission.Permission: ['id'],
-        rfidsecuritysvc.model.media_perm.MediaPerm: ['id'],
-    }
-    monkeypatch.setattr(rfidsecuritysvc.model.BaseModel, '_read_only_keys', _read_only_keys, raising=False)
-    monkeypatch.setattr(rfidsecuritysvc.model.BaseModel, 'to_json_rw', to_json_rw, raising=False)
-
-    for c, read_only_attrs in models_to_patch.items():
-        monkeypatch.setattr(c, '_read_only_keys', lambda _: read_only_attrs, raising=False)
