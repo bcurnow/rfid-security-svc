b41b7a7a625a1de710fb1be5cfb6fbf9c957e51d
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-11 00:32:24 +0000

    Fixed put logic (put gets passed a body, not individual parameters) and the message for post.

--- rfidsecuritysvc/api/guest.py
+++ rfidsecuritysvc/api/guest.py
@@ -23,17 +23,17 @@ def post(body):
         model.create(**body)
         return None, 201
     except exception.DuplicateGuestError:
-        return f'Object with id "{body["id"]}" or first_name "{body["first_name"]}" and last_name "{body["last_name"]}" already exists.', 409
+        return f'Object with first_name "{body["first_name"]}" and last_name "{body["last_name"]}" already exists.', 409
 
 
 def delete(id):
     return None, 200, {RECORD_COUNT_HEADER: model.delete(id)}
 
 
-def put(first_name, last_name):
+def put(id, body):
     try:
         # Since there are no additional fields beyond first_name and last_name, assume this is a create
-        model.create(first_name, last_name)
+        model.create(body['first_name'], body['last_name'])
         return None, 201, {RECORD_COUNT_HEADER: 1}
     except exception.DuplicateGuestError:
         # This already exists so simply return a 200 because there's nothing else to update
