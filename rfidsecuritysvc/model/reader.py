fd9038fea5972f6c8553c78bfe1747657bd0764a
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-11 01:53:05 +0000

    Removing debug print calls

--- rfidsecuritysvc/model/reader.py
+++ rfidsecuritysvc/model/reader.py
@@ -14,7 +14,6 @@ def read(timeout):
     # Need to grab the device before reading, we're assuming we're running both the API and the main reader loop
     # on the same host and therefore, the main reader loop would normally read the tag before we could.
     # Grabbing the device makes it exclusive to our process and allows us to read the next tag scanned.
-    print(reader.device)
     reader.device.grab()
     try:
         return reader.read(timeout)
