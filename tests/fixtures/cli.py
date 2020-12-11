afee634cf98b34590464d61f3528a170efbffd0d
Brian Curnow <bcurnow@users.noreply.github.com>
2020-12-11 01:53:48 +0000

    Added logic to print the traceback if there's an exception to help with debugging

--- tests/fixtures/cli.py
+++ tests/fixtures/cli.py
@@ -1,4 +1,6 @@
 import pytest
+import traceback
+
 
 import click
 
@@ -6,6 +8,8 @@ import click
 @pytest.fixture(scope='session')
 def assert_output():
     def assert_output(result, msg, exit_code=0, **style):
+        if result.exception:
+            traceback.print_exception(type(result.exception), result.exception, result.exception.__traceback__)
         assert result.exit_code == exit_code
         if style:
             assert bytes(click.style(str(msg), **style), 'UTF-8') in result.stdout_bytes
