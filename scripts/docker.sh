a5d652875a575e3492b766312aba88f311b8125c
“Brian <bcurnow@users.noreply.github.com>
2021-09-01 17:37:50 +0100

    Added volume mount

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -2,12 +2,12 @@
 
 rootDir=$(dirname $0)
 rootDir=$(cd ${rootDir}/.. && pwd)
-targetDir=$(cd ${rootDir} && basename $(pwd))
+imageName=$(cd ${rootDir} && basename $(pwd))
 
 if [[ -e /dev/input/rfid && -c /dev/input/rfid ]]
 then
-	docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind --device=/dev/input/rfid:/dev/input/rfid:r ${targetDir}:latest /bin/bash
+	docker run -it -p 5000:5000 --mount src=rfid-db,target=/rfid-db --mount src="${rootDir}",target=/rfid-security-svc,type=bind --device=/dev/input/rfid:/dev/input/rfid:r ${imageName}:latest /bin/bash
 else
 	echo "Did not find /dev/input/rfid, skipping --device"
-	docker run -it -p 5000:5000 --mount src="${rootDir}",target=/${targetDir},type=bind ${targetDir}:latest /bin/bash
+	docker run -it -p 5000:5000 --mount src=rfid-db,target=/rfid-db --mount src="${rootDir}",target=/rfid-security-svc,type=bind ${imageName}:latest /bin/bash
 fi
