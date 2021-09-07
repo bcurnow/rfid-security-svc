89f8fa2f3eb3016da7193a237376019f5ba3ec82
“Brian <bcurnow@users.noreply.github.com>
2021-09-07 23:20:16 +0100

    updated to use the RFID_SERVICE_URL_CONFIG_KEY

--- rfidsecuritysvc/cli/reader.py
+++ rfidsecuritysvc/cli/reader.py
@@ -3,7 +3,7 @@ from flask.cli import AppGroup
 
 from rfidsecuritysvc.cli.config import list
 from rfidsecuritysvc.model import config
-from rfidsecuritysvc.model.reader import RFID_DEVICE_CONFIG_KEY
+from rfidsecuritysvc.model.reader import RFID_SERVICE_URL_CONFIG_KEY
 
 group = AppGroup('reader')
 
@@ -12,17 +12,17 @@ def register(app):
     app.cli.add_command(group)
 
 
-@group.command('set-device-name')
-@click.argument('name')
+@group.command('set-url')
+@click.argument('url')
 @click.option('--yes', is_flag=True)
 @click.pass_context
-def set_device_name(ctx, name, yes):
-    """Sets the appropriate config key for the reader device name"""
-    c = config.get(RFID_DEVICE_CONFIG_KEY)
+def set_url(ctx, url, yes):
+    """Sets the appropriate config key for the reader URL"""
+    c = config.get(RFID_SERVICE_URL_CONFIG_KEY)
     if c:
         if not yes:
-            click.confirm(f'There is already a key "{c.key}" with value "{c.value}", do you want to replace with "{name}"?', abort=True)
-        config.update(RFID_DEVICE_CONFIG_KEY, name)
+            click.confirm(f'There is already a key "{c.key}" with value "{c.value}", do you want to replace with "{url}"?', abort=True)
+        config.update(RFID_SERVICE_URL_CONFIG_KEY, url)
     else:
-        config.create(RFID_DEVICE_CONFIG_KEY, name)
+        config.create(RFID_SERVICE_URL_CONFIG_KEY, url)
     ctx.invoke(list)
--- tests/cli/test_reader.py
+++ tests/cli/test_reader.py
@@ -1,59 +1,61 @@
 from unittest.mock import patch
 
 from rfidsecuritysvc.model.config import Config
-from rfidsecuritysvc.model.reader import RFID_DEVICE_CONFIG_KEY
+from rfidsecuritysvc.model.reader import RFID_SERVICE_URL_CONFIG_KEY
 
 
 @patch('rfidsecuritysvc.cli.reader.list')
 @patch('rfidsecuritysvc.cli.reader.config')
-def test_set_device_name(config, list, runner):
+def test_set_url(config, list, runner):
     config.get.return_value = None
     config.create.return_value = None
     list.return_value = []
-    runner.invoke(args=['reader', 'set-device-name', '/dev/test'], color=True)
-    config.get.assert_called_once_with(RFID_DEVICE_CONFIG_KEY)
-    config.create.assert_called_once_with(RFID_DEVICE_CONFIG_KEY, '/dev/test')
+    runner.invoke(args=['reader', 'set-url', 'http://localhost:8080/get_uid'], color=True)
+    config.get.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY)
+    config.create.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY, 'http://localhost:8080/get_uid')
     list.assert_called_once()
 
 
 @patch('rfidsecuritysvc.cli.reader.list')
 @patch('rfidsecuritysvc.cli.reader.config')
-def test_set_device_name_alreadyexists(config, list, runner, assert_output):
-    config.get.return_value = Config(RFID_DEVICE_CONFIG_KEY, '/dev/othervalue')
+def test_set_url_alreadyexists(config, list, runner, assert_output):
+    config.get.return_value = Config(RFID_SERVICE_URL_CONFIG_KEY, 'http://otherurl/get_uid')
     config.update.return_value = 1
-    list.return_value = [Config(RFID_DEVICE_CONFIG_KEY, '/dev/test')]
-    result = runner.invoke(args=['reader', 'set-device-name', '/dev/test'], input='y', color=True)
+    list.return_value = [Config(RFID_SERVICE_URL_CONFIG_KEY, 'http://localhost:8080/get_uid')]
+    result = runner.invoke(args=['reader', 'set-url', 'http://localhost:8080/get_uid'], input='y', color=True)
     assert_output(result,
-                  f'There is already a key "{RFID_DEVICE_CONFIG_KEY}" with value "/dev/othervalue", do you want to replace with "/dev/test"?',
+                  f'There is already a key "{RFID_SERVICE_URL_CONFIG_KEY}" '
+                  'with value "http://otherurl/get_uid", do you want to replace with "http://localhost:8080/get_uid"?',
                   0)
-    config.get.assert_called_once_with(RFID_DEVICE_CONFIG_KEY)
-    config.update.assert_called_once_with(RFID_DEVICE_CONFIG_KEY, '/dev/test')
+    config.get.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY)
+    config.update.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY, 'http://localhost:8080/get_uid')
     list.assert_called_once()
 
 
 @patch('rfidsecuritysvc.cli.reader.list')
 @patch('rfidsecuritysvc.cli.reader.config')
-def test_set_device_name_alreadyexists_yes_flag(config, list, runner, assert_output):
-    config.get.return_value = Config(RFID_DEVICE_CONFIG_KEY, '/dev/othervalue')
+def test_set_url_alreadyexists_yes_flag(config, list, runner, assert_output):
+    config.get.return_value = Config(RFID_SERVICE_URL_CONFIG_KEY, 'http://otherurl/get_uid')
     config.update.return_value = 1
-    list.return_value = [Config(RFID_DEVICE_CONFIG_KEY, '/dev/test')]
-    result = runner.invoke(args=['reader', 'set-device-name', '/dev/test', '--yes'], color=True)
-    assert f'There is already a key "{RFID_DEVICE_CONFIG_KEY}" ' \
-        'with value "/dev/othervalue", do you want to replace with "/dev/test"?' not in result.output
-    config.get.assert_called_once_with(RFID_DEVICE_CONFIG_KEY)
-    config.update.assert_called_once_with(RFID_DEVICE_CONFIG_KEY, '/dev/test')
+    list.return_value = [Config(RFID_SERVICE_URL_CONFIG_KEY, 'http://localhost:8080/get_uid')]
+    result = runner.invoke(args=['reader', 'set-url', 'http://localhost:8080/get_uid', '--yes'], color=True)
+    assert f'There is already a key "{RFID_SERVICE_URL_CONFIG_KEY}" ' \
+           'with value "http://otherurl/get_uid", do you want to replace with "http://localhost:8080/get_uid"?' not in result.output
+    config.get.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY)
+    config.update.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY, 'http://localhost:8080/get_uid')
     list.assert_called_once()
 
 
 @patch('rfidsecuritysvc.cli.reader.list')
 @patch('rfidsecuritysvc.cli.reader.config')
-def test_set_device_name_alreadyexists_n(config, list, runner, assert_output):
-    config.get.return_value = Config(RFID_DEVICE_CONFIG_KEY, '/dev/othervalue')
-    result = runner.invoke(args=['reader', 'set-device-name', '/dev/test'], input='n', color=True)
+def test_set_url_alreadyexists_n(config, list, runner, assert_output):
+    config.get.return_value = Config(RFID_SERVICE_URL_CONFIG_KEY, 'http://otherurl/get_uid')
+    result = runner.invoke(args=['reader', 'set-url', 'http://localhost:8080/get_uid'], input='n', color=True)
     assert_output(result,
-                  f'There is already a key "{RFID_DEVICE_CONFIG_KEY}" with value "/dev/othervalue", do you want to replace with "/dev/test"?',
+                  f'There is already a key "{RFID_SERVICE_URL_CONFIG_KEY}" '
+                  'with value "http://otherurl/get_uid", do you want to replace with "http://localhost:8080/get_uid"?',
                   1)
-    config.get.assert_called_once_with(RFID_DEVICE_CONFIG_KEY)
+    config.get.assert_called_once_with(RFID_SERVICE_URL_CONFIG_KEY)
     config.create.assert_not_called()
     config.update.assert_not_called()
     list.assert_not_called()
