6a7f75860ceb125333f63a6676f6c340bba9aeab
“Brian <bcurnow@users.noreply.github.com>
2021-09-15 21:48:33 +0100

    Adding production specific build targets

---
+++ scripts/build-prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rootDir=$(dirname $0)
+rootDir=$(cd ${rootDir}/.. && pwd)
+imageName=$(cd ${rootDir} && basename $(pwd))
+
+docker image build \
+  -t ${imageName}:production  \
+  ${rootDir}
---
+++ scripts/prod.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rootDir=$(dirname $0)
+rootDir=$(cd ${rootDir}/.. && pwd)
+imageName=$(cd ${rootDir} && basename $(pwd))
+
+docker run -p 5000:5000 --mount src=rfid-db,target=/rfid-db ${imageName}:production
