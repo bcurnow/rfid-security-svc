3f84f1b13933f1c32ef3fa8b3a59343984f4b75d
“Brian <bcurnow@users.noreply.github.com>
2021-09-15 21:46:20 +0100

    Reordered items, added includes

--- setup.py
+++ setup.py
@@ -5,24 +5,28 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='rfidsecuritysvc',
-    version='1.0.0',
     author='Brian Curnow',
-    author_email='brian.curnow+rfidreader@gmail.com',
-    description='A Flask and Connexion-based REST API and database for managing RFID media and permissions.',
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/bcurnow/rfid-security-svc',
-    packages=find_packages(),
+    author_email='bcurnow@users.noreply.github.com',
     classifiers=[
         'Programming Language :: Python :: 3.9',
-        'License :: OSI Approved :: Apache Software License'
+        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Natural Language :: English',
     ],
-    python_requires='>=3.6',
+    description='A Flask and Connexion-based REST API and database for managing RFID media and permissions.',
     install_requires=[
         'Flask',
         'connexion[swagger-ui]',
     ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    name='rfidsecuritysvc',
+    packages=find_packages(include=['rfidsecuritysvc', 'rfidsecuritysvc.*']),
+    package_data={
+        'rfidsecuritysvc.api': ['api.yaml'],
+        'rfidsecuritysvc.db': ['schema.sql']
+    },
+    python_requires='>=3.6',
+    url='https://github.com/bcurnow/rfid-security-svc',
+    version='1.0.0',
 )
