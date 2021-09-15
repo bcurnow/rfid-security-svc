1b5fae4a6b439cea1a09439397aca0ead656224d
“Brian <bcurnow@users.noreply.github.com>
2021-09-15 23:33:43 +0100

    Updated to only build the dev_image

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,7 @@ rootDir=$(cd ${rootDir}/.. && pwd)
 imageName=$(cd ${rootDir} && basename $(pwd))
 
 docker image build \
+  --target dev_image \
   --build-arg USER_ID=$(id -u ${USER}) \
   --build-arg GROUP_ID=$(id -g ${USER}) \
   --build-arg INPUT_GROUP_ID=$(getent group input | cut -d: -f3) \
