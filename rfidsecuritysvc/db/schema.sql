b7e1f411d3e138bd575f10f91b4747dc73b3f903
“Brian <bcurnow@users.noreply.github.com>
2021-10-06 02:23:22 +0100

    Updated guest to have a default sound and default color

--- rfidsecuritysvc/db/schema.sql
+++ rfidsecuritysvc/db/schema.sql
@@ -3,7 +3,6 @@ PRAGMA foreign_keys = OFF;
 DROP TABLE IF EXISTS config;
 DROP TABLE IF EXISTS guest;
 DROP TABLE IF EXISTS guest_media;
-DROP TABLE IF EXISTS guest_pref;
 DROP TABLE IF EXISTS media;
 DROP TABLE IF EXISTS permission;
 DROP TABLE IF EXISTS sound;
@@ -18,7 +17,22 @@ CREATE TABLE guest (
   id INTEGER PRIMARY KEY,
   first_name TEXT NOT NULL,
   last_name TEXT NOT NULL,
+  default_sound INTEGER,
+  default_color INTEGER,
+  FOREIGN KEY (default_sound) REFERENCES sound (id)
   CONSTRAINT unique_full_name UNIQUE (first_name, last_name)
+);
+
+CREATE TABLE guest_media (
+  id INTEGER PRIMARY KEY,
+  guest_id INTEGER NOT NULL,
+  media_id TEXT NOT NULL,
+  sound INTEGER,
+  color INTEGER,
+  FOREIGN KEY (guest_id) REFERENCES guest (id) ON DELETE CASCADE,
+  FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE,
+  FOREIGN KEY (sound) REFERENCES sound (id)
+  CONSTRAINT only_one_guest_for_media UNIQUE(media_id)
 );
 
 CREATE TABLE media (
@@ -27,27 +41,10 @@ CREATE TABLE media (
   desc TEXT
 );
 
-CREATE TABLE guest_media (
-  id INTEGER PRIMARY KEY,
-  guest_id INTEGER NOT NULL,
-  media_id TEXT NOT NULL,
-  FOREIGN KEY (guest_id) REFERENCES guest (id) ON DELETE CASCADE,
-  FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE,
-  CONSTRAINT only_one_guest_for_media UNIQUE(media_id)
-);
-
 CREATE TABLE permission (
   id INTEGER PRIMARY KEY,
   name TEXT UNIQUE NOT NULL,
   desc TEXT
-);
-
-CREATE TABLE guest_pref (
-  guest_id INTEGER NOT NULL,
-  name TEXT PRIMARY KEY,
-  value TEXT NOT NULL,
-  FOREIGN KEY (guest_id) REFERENCES guest (id) ON DELETE CASCADE,
-  CONSTRAINT unique_guest_pref UNIQUE (guest_id, name)
 );
 
 CREATE TABLE media_perm (
