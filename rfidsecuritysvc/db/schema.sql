0ae32acc0b2cf80d37fa03ce56ebc38324237ede
“Brian <bcurnow@users.noreply.github.com>
2021-10-07 02:07:59 +0100

    Updated perm_id to permission_id to be consistent with the code base

--- rfidsecuritysvc/db/schema.sql
+++ rfidsecuritysvc/db/schema.sql
@@ -50,10 +50,10 @@ CREATE TABLE permission (
 CREATE TABLE media_perm (
   id INTEGER PRIMARY KEY,
   media_id TEXT NOT NULL,
-  perm_id INTEGER NOT NULL,
+  permission_id INTEGER NOT NULL,
   FOREIGN KEY (media_id) REFERENCES media (id) ON DELETE CASCADE,
-  FOREIGN KEY (perm_id) REFERENCES permission (id) ON DELETE CASCADE,
-  CONSTRAINT unique_association UNIQUE (media_id, perm_id)
+  FOREIGN KEY (permission_id) REFERENCES permission (id) ON DELETE CASCADE,
+  CONSTRAINT unique_association UNIQUE (media_id, permission_id)
 );
 
 CREATE TABLE sound (
