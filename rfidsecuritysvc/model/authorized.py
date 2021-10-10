88d5438254fce964cd837c355c92af72746a923c
“Brian <bcurnow@users.noreply.github.com>
2021-10-10 18:47:55 +0100

    Updated to return a MediaConfig and properly JSON serialize it

--- rfidsecuritysvc/model/authorized.py
+++ rfidsecuritysvc/model/authorized.py
@@ -1,34 +1,34 @@
-from rfidsecuritysvc import exception as exception
 from rfidsecuritysvc.model import BaseModel, media_perm
-from rfidsecuritysvc.model.guest import Guest
 
 
 class MediaConfig(BaseModel):
     def __init__(self, media_perm, guest, sound_id, sound_name, color):
-        self.media_id = media_perm.media_id
-        self.media_name = media_perm.media_name
-        self.media_desc = media_perm.media_desc
-        self.permission_id = media_perm.permission_id
-        self.permission_name = media_perm.permission_name
-        self.permission_desc = media_perm.permission_desc
-        self.guest_id = guest.id
-        self.guest_first_name = guest.first_name
-        self.guest_last_name = guest.last_name
+        self.media = media_perm.media
+        self.permission = media_perm.permission
+        self.guest = guest
         self.sound_id = sound_id
         self.sound_name = sound_name
         self.color = color
+        if color is not None:
+            # The color is stored as an integer, convert it to a hex string (e.g. FFFFFF)
+            # and an HTML hex string (e.g. #ffffff) for use in various contexts
+            self.color_hex = format(color, 'X')
+            self.color_html = f'#{format(color, "x")}'
+        else:
+            self.color_hex = None
+            self.color_html = None
+
+    def to_json(self):
+        copy = self.__dict__.copy()
+        copy['media'] = self.media.__dict__.copy()
+        copy['permission'] = self.permission.__dict__.copy()
+        if self.guest is not None:
+            copy['guest'] = self.guest.__dict__.copy()
+        return copy
 
 
 def authorized(media_id, perm_name):
-    try:
-        mp = media_perm.get_by_media_and_perm(media_id, perm_name)
-        if mp:
-            return MediaConfig(mp,
-                               Guest(-1, 'not yet implemented', 'not yet implemented'),
-                               -1,
-                               'not yet implemented',
-                               0x000000)
-    except exception.PermissionNotFoundError:
-        return None
-    except exception.MediaPermNotFoundError:
-        return None
+    mp = media_perm.get_by_media_and_perm(media_id, perm_name)
+    if not mp:
+        return
+    return MediaConfig(mp, None, None, None, None)
--- tests/model/test_authorized.py
+++ tests/model/test_authorized.py
@@ -3,30 +3,63 @@ from unittest.mock import patch
 import rfidsecuritysvc.model.authorized as model
 from rfidsecuritysvc.model.guest import Guest
 from rfidsecuritysvc.model.authorized import MediaConfig
-from rfidsecuritysvc.exception import PermissionNotFoundError, MediaPermNotFoundError
 
 
 @patch('rfidsecuritysvc.model.authorized.media_perm')
 def test_authorized(media_perm, media_perms):
     media_perm.get_by_media_and_perm.return_value = media_perms[0]
-    expected = MediaConfig(media_perms[0],
-                           Guest(-1, 'not yet implemented', 'not yet implemented'),
-                           -1,
-                           'not yet implemented',
-                           0x000000)
+    expected = MediaConfig(media_perms[0], None, None, None, None)
     assert model.authorized('test', 'test_perm') == expected
     media_perm.get_by_media_and_perm.assert_called_once_with('test', 'test_perm')
 
 
 @patch('rfidsecuritysvc.model.authorized.media_perm')
-def test_authorized_invalid_perm_name(media_perm):
-    media_perm.get_by_media_and_perm.side_effect = PermissionNotFoundError
-    assert model.authorized('test', 'test_perm') is None
+def test_authorized_not_found(media_perm, media_perms):
+    media_perm.get_by_media_and_perm.return_value = None
+    assert model.authorized('test', 'test_perm') == None
     media_perm.get_by_media_and_perm.assert_called_once_with('test', 'test_perm')
 
 
-@patch('rfidsecuritysvc.model.authorized.media_perm')
-def test_authorized_invalid_media_id(media_perm):
-    media_perm.get_by_media_and_perm.side_effect = MediaPermNotFoundError
-    assert model.authorized('test', 'test_perm') is None
-    media_perm.get_by_media_and_perm.assert_called_once_with('test', 'test_perm')
+def test_MediaConfig(media_perms):
+    guest = Guest(1, 'first_name', 'last_name')
+    mc = MediaConfig(media_perms[0], guest, 2, 'test.wav', 0xABCDEF0123456789)
+    assert mc.media == media_perms[0].media
+    assert mc.permission == media_perms[0].permission
+    assert mc.guest == guest
+    assert mc.sound_id == 2
+    assert mc.sound_name == 'test.wav'
+    assert mc.color == 0xABCDEF0123456789
+    assert mc.color_hex == 'ABCDEF0123456789'
+    assert mc.color_html == '#abcdef0123456789'
+
+
+def test_MediaConfig_no_color(media_perms):
+    mc = MediaConfig(media_perms[0], None, None, None, None)
+    assert mc.color is None
+    assert mc.color_hex is None
+    assert mc.color_html is None
+
+
+def test_MediaConfig_no_sound(media_perms):
+    mc = MediaConfig(media_perms[0], None, None, None, None)
+    assert mc.sound_id == None
+    assert mc.sound_name == None
+
+
+def test_MediaConfig_no_guest(media_perms):
+    mc = MediaConfig(media_perms[0], None, None, None, None)
+    assert mc.guest == None
+
+
+def test_MediaConfig_to_json(media_perms):
+    guest = Guest(1, 'first_name', 'last_name')
+    mc = MediaConfig(media_perms[0], guest, 2, 'test.wav', 0xABCDEF0123456789)
+    json = mc.to_json()
+    assert json['media'] == media_perms[0].media.to_json()
+    assert json['permission'] == media_perms[0].permission.to_json()
+    assert json['guest'] == guest.to_json()
+    assert json['sound_id'] == 2
+    assert json['sound_name'] == 'test.wav'
+    assert json['color'] == 0xABCDEF0123456789
+    assert json['color_hex'] == 'ABCDEF0123456789'
+    assert json['color_html'] == '#abcdef0123456789'
