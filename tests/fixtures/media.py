341a385cad835185a5bffb6cab7650d798368d9b
“Brian <bcurnow@users.noreply.github.com>
2021-10-10 20:14:39 +0100

    Adding guest fixtures to support guest_media

--- tests/fixtures/media.py
+++ tests/fixtures/media.py
@@ -15,6 +15,7 @@ def medias():
         Media('TEST MEDIA 5', 'test media 5', 'Media for testing (5)'),
         Media('TEST OPEN DOOR', 'test open door', 'This media will be assigned the permission Open Door'),
         Media('TEST WITHOUT DESC', 'test without desc', None),
+        Media('TEST FOR GUESTMEDIA CREATION', 'test for GuestMedia creation', None)
     ]
 
 
@@ -25,6 +26,13 @@ def creatable_media():
 
 @pytest.fixture(scope='session')
 def no_desc_media(medias):
+    return medias[7]
+
+
+@pytest.fixture(scope='session')
+def no_prefs_media(medias):
+    """ For use when building GuestMedia records """
+    # Use the no_desc_media
     return medias[7]
 
 
@@ -40,8 +48,20 @@ def not_authorized_media(medias):
 
 @pytest.fixture(scope='session')
 def media_for_creatable_media_perm(medias):
-    """ This media should be used when creating a creable media_perm record."""
+    """ This media should be used when creating a media_perm record."""
     return medias[4]
+
+
+@pytest.fixture(scope='session')
+def media_for_creatable_guest_media(medias):
+    """ This media should be used when creating a guest_media record."""
+    return medias[8]
+
+
+@pytest.fixture(scope='session')
+def media_for_guests(medias):
+    """ Each media returned should be associated with a guest """
+    return medias[1:5]
 
 
 @pytest.fixture(autouse=True, scope='session')
