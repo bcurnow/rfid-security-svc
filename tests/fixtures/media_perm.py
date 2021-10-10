5510c4e6e5c2b71afe41ec2f2e3717b708c9db8e
“Brian <bcurnow@users.noreply.github.com>
2021-10-10 18:54:59 +0100

    Updated to reflect new composite MediaPerm and added test_create and test_update methods

--- tests/fixtures/media_perm.py
+++ tests/fixtures/media_perm.py
@@ -9,29 +9,39 @@ def media_perms(medias, open_door_media, open_door_permission, default_permissio
     media_perms = []
     for i in range(len(medias)):
         if medias[i] != not_authorized_media:
-            media_perms.append(MediaPerm(i,
-                                         medias[i].id,
-                                         medias[i].name,
-                                         medias[i].desc,
-                                         default_permission.id,
-                                         default_permission.name,
-                                         default_permission.desc))
-    media_perms.append(MediaPerm(len(media_perms) + 1,
-                                 open_door_media.id,
-                                 open_door_media.name,
-                                 open_door_media.desc,
-                                 open_door_permission.id,
-                                 open_door_permission.name,
-                                 open_door_permission.desc))
+            media_perms.append(MediaPerm(i, medias[i], default_permission))
+    media_perms.append(MediaPerm(len(media_perms) + 1, open_door_media, open_door_permission))
     return media_perms
 
 
 @pytest.fixture(scope='session')
 def creatable_media_perm(media_perms, media_for_creatable_media_perm, permission_for_creatable_media_perm):
-    return MediaPerm(len(media_perms) + 1,
-                     media_for_creatable_media_perm.id,
-                     media_for_creatable_media_perm.name,
-                     media_for_creatable_media_perm.desc,
-                     permission_for_creatable_media_perm.id,
-                     permission_for_creatable_media_perm.name,
-                     permission_for_creatable_media_perm.desc)
+    return MediaPerm(len(media_perms) + 1, media_for_creatable_media_perm, permission_for_creatable_media_perm)
+
+
+@pytest.fixture(scope='session')
+def media_perm_to_row():
+    def to_row(m):
+        row = {}
+        row['id'] = m.id
+        row['media_id'] = m.media.id
+        row['media_name'] = m.media.name
+        row['media_desc'] = m.media.desc
+        row['permission_id'] = m.permission.id
+        row['permission_name'] = m.permission.name
+        row['permission_desc'] = m.permission.desc
+        return row
+
+    return to_row
+
+
+@pytest.fixture(autouse=True, scope='session')
+def add_media_perm_helpers(monkeypatch_session):
+    def convert(self):
+        return {
+            'media_id': self.media.id,
+            'permission_id': self.permission.id,
+        }
+
+    monkeypatch_session.setattr(MediaPerm, 'test_create', convert, raising=False)
+    monkeypatch_session.setattr(MediaPerm, 'test_update', convert, raising=False)
