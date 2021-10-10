467947e406304a7ae95bd9ad5fbbce5d8e6c87db
“Brian <bcurnow@users.noreply.github.com>
2021-10-10 18:53:45 +0100

    added test_create and test_update

--- tests/fixtures/config.py
+++ tests/fixtures/config.py
@@ -12,3 +12,12 @@ def configs(test_api_key):
 @pytest.fixture(scope='session')
 def creatable_config():
     return Config('creatable key', 'creatable value')
+
+
+@pytest.fixture(autouse=True, scope='session')
+def add_config_helpers(monkeypatch_session):
+    def convert(self):
+        return self.__dict__.copy()
+
+    monkeypatch_session.setattr(Config, 'test_create', convert, raising=False)
+    monkeypatch_session.setattr(Config, 'test_update', convert, raising=False)
