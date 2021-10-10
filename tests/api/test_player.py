747058c8f86daf711ae03797a24aef2aca4004d2
“Brian <bcurnow@users.noreply.github.com>
2021-10-10 18:51:17 +0100

    Updated test name to match the rest of the code base

--- tests/api/test_player.py
+++ tests/api/test_player.py
@@ -1,10 +1,11 @@
+""" This only contains the not_found test as the others require the app context and are found in api-client """
 from unittest.mock import patch
 
 from rfidsecuritysvc.api import player as api
 
 
 @patch('rfidsecuritysvc.api.player.model')
-def test_get_notfound(model):
+def test_get_not_found(model):
     model.get_by_name.return_value = None
     assert api.get('notfound') == ('Object with name "notfound" does not exist.', 404)
     model.get_by_name.assert_called_once_with('notfound')
