de9967110729100b232593c5694e36abf2e1b75d
“Brian <bcurnow@users.noreply.github.com>
2021-10-15 04:27:58 +0100

    Converted default_sound and default_color to objects, removed the default_ prefix as this wasn't really adding anything and it was inconsistent with guest_media

--- rfidsecuritysvc/model/guest.py
+++ rfidsecuritysvc/model/guest.py
@@ -1,24 +1,25 @@
 from rfidsecuritysvc.db import guest as table
 from rfidsecuritysvc import exception
-from rfidsecuritysvc.model import BaseModel, sound
+from rfidsecuritysvc.model import BaseModel, sound as sound_model
+from rfidsecuritysvc.model.sound import Sound, to_sound_timestamp
+from rfidsecuritysvc.model.color import Color
 
 
 class Guest(BaseModel):
-    def __init__(self, id, first_name, last_name, default_sound=None, default_sound_name=None, default_color=None):
+    def __init__(self, id, first_name, last_name, sound=None, color=None):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
-        self.default_sound = default_sound
-        self.default_sound_name = default_sound_name
-        self.default_color = default_color
-        if default_color is not None:
-            # The default_color is stored as an integer, convert it to a hex string (e.g. FFFFFF)
-            # and an HTML hex string (e.g. #ffffff) for use in various contexts
-            self.default_color_hex = '{:X}'.format(default_color)
-            self.default_color_html = f'#{"{:06x}".format(default_color)}'
-        else:
-            self.default_color_hex = None
-            self.default_color_html = None
+        self.sound = sound
+        self.color = color
+
+    def to_json(self):
+        copy = super().to_json()
+        if self.sound:
+            copy['sound'] = self.sound.to_json()
+        if self.color:
+            copy['color'] = self.color.to_json()
+        return copy
 
 
 def get(id):
@@ -33,34 +34,41 @@ def list():
     return result
 
 
-def create(first_name, last_name, default_sound=None, default_color=None):
-    if default_sound:
-        s = sound.get(default_sound)
+def create(first_name, last_name, sound=None, color=None):
+    if sound:
+        s = sound_model.get(sound)
         if not s:
             raise exception.SoundNotFoundError
-    return table.create(first_name, last_name, default_sound, default_color)
+    return table.create(first_name, last_name, sound, color)
 
 
 def delete(id):
     return table.delete(id)
 
 
-def update(id, first_name, last_name, default_sound=None, default_color=None):
-    if default_sound:
-        s = sound.get(default_sound)
+def update(id, first_name, last_name, sound=None, color=None):
+    if sound:
+        s = sound_model.get(sound)
         if not s:
             raise exception.SoundNotFoundError
-    return table.update(id, first_name, last_name, default_sound, default_color)
+    return table.update(id, first_name, last_name, sound, color)
 
 
 def __model(row):
     if not row:
         return
+
+    color = None
+    if row['color'] is not None:
+        color = Color(row['color'])
+
+    sound = None
+    if row['sound'] is not None:
+        sound = Sound(row['sound'], row['sound_name'], to_sound_timestamp(row['sound_last_update_timestamp']))
+
     return Guest(
         row['id'],
         row['first_name'],
         row['last_name'],
-        row['default_sound'],
-        row['default_sound_name'],
-        row['default_color'],
-        )
+        sound,
+        color)
--- tests/model/test_guest.py
+++ tests/model/test_guest.py
@@ -2,39 +2,20 @@ import pytest
 from unittest.mock import patch
 
 import rfidsecuritysvc.model.guest as model
+from rfidsecuritysvc.model.color import Color
 from rfidsecuritysvc.model.guest import Guest
+from rfidsecuritysvc.model.sound import Sound
 from rfidsecuritysvc.exception import SoundNotFoundError
 
 
-def test_Guest(assert_model, default_sound):
-    assert_model(_model(1, 'first', 'last', default_sound.id, default_sound.name, 0xABCDEF),
-                 Guest(1, 'first', 'last', default_sound.id, default_sound.name, 0xABCDEF))
-
-
-def test_Guest___init__(assert_model, default_sound):
-    g = Guest(1, 'first', 'last', default_sound.id, default_sound.name, 0xABCDEF)
-    assert g.default_color == 11259375
-    assert g.default_color_hex == 'ABCDEF'
-    assert g.default_color_html == '#abcdef'
-
-
-def test_Guest___init___no_color(assert_model, default_sound):
-    g = Guest(1, 'first', 'last', default_sound.id, default_sound.name, None)
-    assert g.default_color is None
-    assert g.default_color_hex is None
-    assert g.default_color_html is None
-
-
-def test_Guest___init___zero_color(assert_model, default_sound):
-    g = Guest(1, 'first', 'last', default_sound.id, default_sound.name, 0)
-    assert g.default_color == 0
-    assert g.default_color_hex == '0'
-    assert g.default_color_html == '#000000'
+def test_Guest(assert_model, default_sound, default_color):
+    assert_model(_model(1, 'first', 'last', default_sound, default_color),
+                 Guest(1, 'first', 'last', default_sound, default_color))
 
 
 @patch('rfidsecuritysvc.model.guest.table')
 def test_get(table):
-    table.get.return_value = _default().to_json()
+    table.get.return_value = _default().test_to_row()
     assert model.get(1) == _default()
     table.get.assert_called_once_with(1)
 
@@ -49,8 +30,8 @@ def test_get_notfound(table):
 @patch('rfidsecuritysvc.model.guest.table')
 def test_list(table):
     table.list.return_value = [
-        _default().to_json(),
-        _default(2).to_json(),
+        _default().test_to_row(),
+        _default(2).test_to_row(),
     ]
     models = model.list()
     table.list.assert_called_once()
@@ -65,7 +46,7 @@ def test_list_noresults(table):
     assert models == []
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_create(table, sound, default_sound):
     sound.get.return_value = default_sound
@@ -75,7 +56,7 @@ def test_create(table, sound, default_sound):
     table.create.assert_called_once_with('first', 'last', default_sound.id, 0xABCDEF)
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_create_SoundNotFoundError(table, sound, default_sound):
     sound.get.return_value = None
@@ -85,7 +66,7 @@ def test_create_SoundNotFoundError(table, sound, default_sound):
     table.create.assert_not_called()
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_create_no_prefs(table, sound, default_sound):
     table.create.return_value = None
@@ -101,7 +82,7 @@ def test_delete(table):
     table.delete.assert_called_with(1)
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_update(table, sound, default_sound):
     sound.get.return_value = default_sound
@@ -111,7 +92,7 @@ def test_update(table, sound, default_sound):
     table.update.assert_called_once_with(1, 'first', 'last', default_sound.id, 0xABCDEF)
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_update_no_prefs(table, sound, default_sound):
     table.update.return_value = 1
@@ -120,7 +101,7 @@ def test_update_no_prefs(table, sound, default_sound):
     table.update.assert_called_once_with(1, 'first', 'last', None, None)
 
 
-@patch('rfidsecuritysvc.model.guest.sound')
+@patch('rfidsecuritysvc.model.guest.sound_model')
 @patch('rfidsecuritysvc.model.guest.table')
 def test_update_SoundNotFoundError(table, sound, default_sound):
     table.update.return_value = 1
@@ -131,9 +112,23 @@ def test_update_SoundNotFoundError(table, sound, default_sound):
     table.update.assert_not_called()
 
 
+def test__model_no_color(creatable_guest):
+    row = creatable_guest.test_to_row()
+    row['color'] = None
+    g = model.__model(row)
+    assert g.color is None
+
+
+def test__model_no_sound(creatable_guest):
+    row = creatable_guest.test_to_row()
+    row['sound'] = None
+    g = model.__model(row)
+    assert g.sound is None
+
+
 def _default(index=1):
-    return _model(index, f'first {index}', f'last {index}', index, f'sound_name {index}', 0xABCDEF)
+    return _model(index, f'first {index}', f'last {index}', Sound(index, f'sound_name {index}'), Color(0xABCDEF))
 
 
-def _model(id, first_name, last_name, default_sound, default_sound_name, default_color):
-    return Guest(id, first_name, last_name, default_sound, default_sound_name, default_color)
+def _model(id, first_name, last_name, sound, color):
+    return Guest(id, first_name, last_name, sound, color)
