b839c3c2c04588b2ea5d89129a528205cf5f6bb7
“Brian <bcurnow@users.noreply.github.com>
2021-10-15 04:27:07 +0100

    Converted sound and color to objects, updated the resolution logic

--- rfidsecuritysvc/model/authorized.py
+++ rfidsecuritysvc/model/authorized.py
@@ -2,21 +2,12 @@ from rfidsecuritysvc.model import BaseModel, media_perm, guest_media
 
 
 class MediaConfig(BaseModel):
-    def __init__(self, media_perm, guest, sound_id, sound_name, color):
+    def __init__(self, media_perm, guest, sound, color):
         self.media = media_perm.media
         self.permission = media_perm.permission
         self.guest = guest
-        self.sound_id = sound_id
-        self.sound_name = sound_name
+        self.sound = sound
         self.color = color
-        if color is not None:
-            # The color is stored as an integer, convert it to a hex string (e.g. FFFFFF)
-            # and an HTML hex string (e.g. #ffffff) for use in various contexts
-            self.color_hex = format(color, 'X')
-            self.color_html = f'#{format(color, "x")}'
-        else:
-            self.color_hex = None
-            self.color_html = None
 
     def to_json(self):
         copy = super().to_json()
@@ -24,6 +15,10 @@ class MediaConfig(BaseModel):
         copy['permission'] = self.permission.to_json()
         if self.guest is not None:
             copy['guest'] = self.guest.to_json()
+        if self.sound is not None:
+            copy['sound'] = self.sound.to_json()
+        if self.color is not None:
+            copy['color'] = self.color.to_json()
         return copy
 
 
@@ -36,24 +31,23 @@ def authorized(media_id, perm_name):
 
     color = None
     guest = None
-    sound_id = None
-    sound_name = None
+    sound = None
     if gm:
         color = _resolveColor(gm)
         guest = gm.guest
-        sound_id, sound_name = _resolveSound(gm)
+        sound = _resolveSound(gm)
 
-    return MediaConfig(mp, guest, sound_id, sound_name, color)
+    return MediaConfig(mp, guest, sound, color)
 
 
 def _resolveColor(gm):
     if gm.color:
         return gm.color
     # This can also be None but that's OK
-    return gm.guest.default_color
+    return gm.guest.color
 
 
 def _resolveSound(gm):
-    if gm.sound_id:
-        return (gm.sound_id, gm.sound_name)
-    return (gm.guest.default_sound, gm.guest.default_sound_name)
+    if gm.sound:
+        return gm.sound
+    return gm.guest.sound
--- tests/model/test_authorized.py
+++ tests/model/test_authorized.py
@@ -1,10 +1,13 @@
 from unittest.mock import patch
 
 import rfidsecuritysvc.model.authorized as model
+from rfidsecuritysvc.model.color import Color
 from rfidsecuritysvc.model.guest import Guest
 from rfidsecuritysvc.model.guest_media import GuestMedia
 from rfidsecuritysvc.model.media import Media
 from rfidsecuritysvc.model.authorized import MediaConfig
+from rfidsecuritysvc.model.sound import Sound
+
 
 
 @patch('rfidsecuritysvc.model.authorized.guest_media')
@@ -14,8 +17,7 @@ def test_authorized(media_perm, guest_media, open_door_media_perm, open_door_gue
     guest_media.get_by_media.return_value = open_door_guest_media
     expected = MediaConfig(open_door_media_perm,
                            open_door_guest_media.guest,
-                           open_door_guest_media.sound_id,
-                           open_door_guest_media.sound_name,
+                           open_door_guest_media.sound,
                            open_door_guest_media.color)
     assert model.authorized('test', 'test_perm') == expected
     media_perm.get_by_media_and_perm.assert_called_once_with('test', 'test_perm')
@@ -31,64 +33,62 @@ def test_authorized_not_found(media_perm, media_perms):
 
 def test_MediaConfig(media_perms):
     guest = Guest(1, 'first_name', 'last_name')
-    mc = MediaConfig(media_perms[0], guest, 2, 'test.wav', 0xABCDEF0123456789)
+    sound = Sound(2, 'test.wav')
+    color = Color(0xABCDEF0123456789)
+    mc = MediaConfig(media_perms[0], guest, sound, color)
     assert mc.media == media_perms[0].media
     assert mc.permission == media_perms[0].permission
     assert mc.guest == guest
-    assert mc.sound_id == 2
-    assert mc.sound_name == 'test.wav'
-    assert mc.color == 0xABCDEF0123456789
-    assert mc.color_hex == 'ABCDEF0123456789'
-    assert mc.color_html == '#abcdef0123456789'
+    assert mc.sound == sound
+    assert mc.color == color
 
 
 def test_MediaConfig_no_color(media_perms):
-    mc = MediaConfig(media_perms[0], None, None, None, None)
+    mc = MediaConfig(media_perms[0], None, None, None)
     assert mc.color is None
-    assert mc.color_hex is None
-    assert mc.color_html is None
 
 
 def test_MediaConfig_no_sound(media_perms):
-    mc = MediaConfig(media_perms[0], None, None, None, None)
-    assert mc.sound_id is None
-    assert mc.sound_name is None
+    mc = MediaConfig(media_perms[0], None, None, None)
+    assert mc.sound is None
 
 
 def test_MediaConfig_no_guest(media_perms):
-    mc = MediaConfig(media_perms[0], None, None, None, None)
+    mc = MediaConfig(media_perms[0], None, None, None)
     assert mc.guest is None
 
 
 def test_MediaConfig_to_json(media_perms):
     guest = Guest(1, 'first_name', 'last_name')
-    mc = MediaConfig(media_perms[0], guest, 2, 'test.wav', 0xABCDEF0123456789)
+    sound = Sound(2, 'test.wav')
+    color = Color(0xABCDEF0123456789)
+    mc = MediaConfig(media_perms[0], guest, sound, color)
     json = mc.to_json()
     assert json['media'] == media_perms[0].media.to_json()
     assert json['permission'] == media_perms[0].permission.to_json()
     assert json['guest'] == guest.to_json()
-    assert json['sound_id'] == 2
-    assert json['sound_name'] == 'test.wav'
-    assert json['color'] == 0xABCDEF0123456789
-    assert json['color_hex'] == 'ABCDEF0123456789'
-    assert json['color_html'] == '#abcdef0123456789'
+    assert json['sound'] == sound.to_json()
+    assert json['color'] == color.to_json()
 
 
 def test__resolveColor_from_guest():
     gm = modifiableGuestMedia()
     # Set the color attribute on the guest but leave the rest at None
-    gm.guest.default_color = 0x12345
+    color = Color(0x12345)
+    gm.guest.color = color
 
-    assert model._resolveColor(gm) == 0x12345
+    assert model._resolveColor(gm) == color
 
 
 def test__resolveColor_from_guest_media():
     gm = modifiableGuestMedia()
     # Set the color attribute on both the guest and guest_media to ensure the guest_media value is returned
-    gm.guest.default_color = 0x12345
-    gm.color = 0x6789
+    g_color = Color(0x12345)
+    gm.guest.color = g_color
+    gm_color = Color(0x6789)
+    gm.color = gm_color
 
-    assert model._resolveColor(gm) == 0x6789
+    assert model._resolveColor(gm) == gm_color
 
 
 def test__resolveColor_none():
@@ -99,31 +99,31 @@ def test__resolveColor_none():
 def test__resolveSound_from_guest():
     gm = modifiableGuestMedia()
     # Set the sound attributes on the guest but leave the rest at None
-    gm.guest.default_sound = 1
-    gm.guest.default_sound_name = 'guest sound'
+    sound = Sound(1, 'guest sound')
+    gm.guest.sound = sound
 
-    assert model._resolveSound(gm) == (1, 'guest sound')
+    assert model._resolveSound(gm) == sound
 
 
 def test__resolveSound_from_guest_media():
     gm = modifiableGuestMedia()
     # Set the sound attributes on both the guest and guest_media to ensure the guest_media value is returned
-    gm.guest.default_sound = 1
-    gm.guest.default_sound_name = 'guest sound'
-    gm.sound_id = 2
-    gm.sound_name = 'guest media sound'
+    g_sound = Sound(1, 'guest sound')
+    gm.guest.sound = g_sound
+    gm_sound = Sound(2, 'guest media sound')
+    gm.sound = gm_sound
 
-    assert model._resolveSound(gm) == (2, 'guest media sound')
+    assert model._resolveSound(gm) == gm_sound
 
 
 def test__resolveSound_none():
     gm = modifiableGuestMedia()
-    assert model._resolveSound(gm) == (None, None)
+    assert model._resolveSound(gm) == None
 
 
 def modifiableGuestMedia():
     """ Returns a new GuestMedia object every time it's called to allow for modifications of the object state. """
-    g = Guest(1, 'Mickey', 'Mouse', None, None, None)
+    g = Guest(1, 'Mickey', 'Mouse', None, None)
     m = Media('modifiable', 'modifiable', 'this media is modifiable')
-    gm = GuestMedia(1, g, m, None, None, None)
+    gm = GuestMedia(1, g, m, None, None)
     return gm
