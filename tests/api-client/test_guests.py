dc2b36a7c246836faa9bfc6bddcb66e7cdfd0d4c
“Brian <bcurnow@users.noreply.github.com>
2021-10-15 05:01:19 +0100

    Updated to use the test_create and test_update methods since sound and color are now objects

--- tests/api-client/test_guests.py
+++ tests/api-client/test_guests.py
@@ -28,7 +28,7 @@ def test_search_noresults(model, rh):
 
 
 def test_post(rh, creatable_guest):
-    rh.assert_response(rh.open('post', f'{api}', creatable_guest), 201)
+    rh.assert_response(rh.open('post', f'{api}', creatable_guest.test_create()), 201)
     rh.assert_response(rh.open('get', f'{api}/{creatable_guest.id}'), 200)
     rh.assert_response(rh.open('delete', f'{api}/{creatable_guest.id}'), 200, headers={RECORD_COUNT_HEADER: '1'})
     rh.assert_response(rh.open('get', f'{api}/{creatable_guest.id}'), 404)
@@ -36,18 +36,18 @@ def test_post(rh, creatable_guest):
 
 def test_post_no_prefs(rh, creatable_guest):
     no_prefs = Guest(creatable_guest.id, creatable_guest.first_name, creatable_guest.last_name, None, None)
-    rh.assert_response(rh.open('post', f'{api}', no_prefs), 201)
+    rh.assert_response(rh.open('post', f'{api}', no_prefs.test_create()), 201)
     rh.assert_response(rh.open('get', f'{api}/{no_prefs.id}'), 200)
     rh.assert_response(rh.open('delete', f'{api}/{no_prefs.id}'), 200, headers={RECORD_COUNT_HEADER: '1'})
     rh.assert_response(rh.open('get', f'{api}/{no_prefs.id}'), 404)
 
 
 def test_post_duplicate(rh, guests):
-    rh.assert_response(rh.open('post', f'{api}', guests[0]), 409)
+    rh.assert_response(rh.open('post', f'{api}', guests[0].test_create()), 409)
 
 
 def test_delete(rh, creatable_guest):
-    rh.assert_response(rh.open('post', f'{api}', creatable_guest), 201)
+    rh.assert_response(rh.open('post', f'{api}', creatable_guest.test_create()), 201)
     rh.assert_response(rh.open('get', f'{api}/{creatable_guest.id}'), 200)
     rh.assert_response(rh.open('delete', f'{api}/{creatable_guest.id}'), 200, headers={RECORD_COUNT_HEADER: '1'})
     rh.assert_response(rh.open('get', f'{api}/{creatable_guest.id}'), 404)
@@ -59,14 +59,14 @@ def test_delete_notfound(rh, creatable_guest):
 
 def test_put_no_prefs(rh, creatable_guest):
     updated = Guest(creatable_guest.id, creatable_guest.first_name, creatable_guest.last_name, None, None)
-    rh.assert_response(rh.open('put', f'{api}/{updated.id}', updated), 201, headers={RECORD_COUNT_HEADER: '1'})
+    rh.assert_response(rh.open('put', f'{api}/{updated.id}', updated.test_update()), 201, headers={RECORD_COUNT_HEADER: '1'})
     rh.assert_response(rh.open('delete', f'{api}/{updated.id}'), 200, headers={RECORD_COUNT_HEADER: '1'})
 
 
 def test_put(rh, guests):
-    rh.assert_response(rh.open('put', f'{api}/{guests[0].id}', guests[0]), 200, headers={RECORD_COUNT_HEADER: '1'})
+    rh.assert_response(rh.open('put', f'{api}/{guests[0].id}', guests[0].test_update()), 200, headers={RECORD_COUNT_HEADER: '1'})
 
 
 def test_put_notfound(rh, creatable_guest):
-    rh.assert_response(rh.open('put', f'{api}/{creatable_guest.id}', creatable_guest), 201, headers={RECORD_COUNT_HEADER: '1'})
-    rh.assert_response(rh.open('delete', f'{api}/{creatable_guest.id}', creatable_guest), 200, headers={RECORD_COUNT_HEADER: '1'})
+    rh.assert_response(rh.open('put', f'{api}/{creatable_guest.id}', creatable_guest.test_update()), 201, headers={RECORD_COUNT_HEADER: '1'})
+    rh.assert_response(rh.open('delete', f'{api}/{creatable_guest.id}'), 200, headers={RECORD_COUNT_HEADER: '1'})
