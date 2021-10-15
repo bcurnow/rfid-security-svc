5d50b70619d149fac0414b72420b4e141a059fd0
“Brian <bcurnow@users.noreply.github.com>
2021-10-15 05:18:25 +0100

    updated the logic so that both the hex and html properties will always return a six digit string

--- rfidsecuritysvc/model/color.py
+++ rfidsecuritysvc/model/color.py
@@ -6,5 +6,5 @@ class Color(BaseModel):
         self.int = color
         # The color is stored as an integer, convert it to a hex string (e.g. FFFFFF)
         # and an HTML hex string (e.g. #ffffff) for use in various contexts
-        self.hex = '{:X}'.format(color)
+        self.hex = '{:06X}'.format(color)
         self.html = f'#{"{:06x}".format(color)}'
--- tests/model/test_color.py
+++ tests/model/test_color.py
@@ -11,5 +11,19 @@ def test_Color__init__():
 def test_Color__init___zero():
     c = Color(0)
     assert c.int == 0
-    assert c.hex == '0'
+    assert c.hex == '000000'
     assert c.html == '#000000'
+
+
+def test_Color__init___two_digits():
+    c = Color(0xFF)
+    assert c.int == 0xFF
+    assert c.hex == '0000FF'
+    assert c.html == '#0000ff'
+
+
+def test_Color__init___four_digits():
+    c = Color(0xFFFF)
+    assert c.int == 0xFFFF
+    assert c.hex == '00FFFF'
+    assert c.html == '#00ffff'
