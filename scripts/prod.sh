bd1eef77620ca5d8fb18d71f00e1df7fe77e2464
“Brian <bcurnow@users.noreply.github.com>
2021-10-25 01:48:04 +0100

    Added command line argument handling

--- scripts/prod.sh
+++ scripts/prod.sh
@@ -4,4 +4,4 @@ rootDir=$(dirname $0)
 rootDir=$(cd ${rootDir}/.. && pwd)
 imageName=$(cd ${rootDir} && basename $(pwd))
 
-docker run -p 5000:5000 --mount src=rfid-db,target=/rfid-db ${imageName}:production
+docker run -p 5000:5000 --mount src=rfid-db,target=/rfid-db ${imageName}:production $@
